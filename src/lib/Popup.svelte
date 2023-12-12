5ea862ace74c45d926296b3f3e1e6009c0687672
abe <abrahamgeorge8547@gmail.com>
2023-12-12 13:11:57 +0530

    adding config for popup and dashboard

--- src/lib/Popup.svelte
+++ src/lib/Popup.svelte
@@ -1,38 +1,15 @@
 <script lang="ts">
   import browser from "webextension-polyfill";
   import { onMount } from "svelte";
-  // import Welcome from "./components/popup/Welcome.svelte";
-  // import Home from "./components/popup/Home.svelte";
+  import Welcome from "./components/popup/Welcome.svelte";
+  import Home from "./components/popup/Home.svelte";
   import Logo from "./components/basic/logo.svelte";
   import { isLoggedIn } from "../lib/store/ui.store";
-
   console.log("is logged in status", $isLoggedIn);
-  // export let name: string;
+  let devType = "dashboard";
 
-  // const callBackgroundFunction = async (): Promise<void> => {
-  //   try {
-  //     const response = await browser.runtime.sendMessage({
-  //       action: "someAction",
-  //     });
-  //     console.log(response);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // Fetch color from storage
-  // const fetchColor = async (): Promise<void> => {
-  //   try {
-  //     const result = await browser.storage.local.get(["color"]);
-  //     console.log("Color", result.color);
-  //   } catch (error) {
-  //     console.error("Error fetching color:", error);
-  //   }
-  // };
-
-  // Fetch color on component mount
   onMount(async () => {
-    openFullscreenTab();
+    if (devType != "popup") openFullscreenTab();
   });
 
   const openFullscreenTab = async () => {
@@ -43,17 +20,6 @@
 </script>
 
 <main>
-  <!-- <h1 class="bg-green-800">Hello {name} !!</h1>
-  <button class="bg-green-700" on:click={callBackgroundFunction}>
-    Call Background Function
-  </button> -->
-  <!-- <button
-    class="rounded bg-blue-500 px-4 py-2 text-base text-white"
-    on:click={openFullscreenTab}
-  >
-    Open Fullscreen Tab
-  </button> -->
-
   <div class="w-[380px] h-[520px] bg-[#262C44] overflow-hidden">
     {#if $isLoggedIn}
       <Home />
--- src/lib/components/popup/Welcome.svelte
+++ src/lib/components/popup/Welcome.svelte
@@ -1,15 +1,11 @@
 <script>
-    import { writable } from 'svelte/store';
-    import {isLoggedIn} from "../../store/ui.store"
-    import Eye from "../basic/eye.svelte"
-  
-    const passphrase = writable('');
-  
-    const hardcodedPassword = "test"; 
+  import { writable } from "svelte/store";
+  import { isLoggedIn } from "../../store/ui.store";
+  import Eye from "../basic/eye.svelte";
 
   const passphrase = writable("");
 
-  const hardcodedPassword = "wonderwomen";
+  const hardcodedPassword = "test";
 
   let showPassword = false;
   let errorMessage = false;
@@ -21,36 +17,38 @@
     const enteredPassphrase = $passphrase;
     if (enteredPassphrase === hardcodedPassword) {
       isLoggedIn.set(true);
-      console.log("isLoggedIn set to true");
     } else {
       errorMessage = true;
     }
-    async function handleSubmit() {
-      const enteredPassphrase = $passphrase;
-      if (enteredPassphrase === hardcodedPassword) {
-        isLoggedIn.set(true);
-      } else {
-        errorMessage = true;
-      }
-    }
-  </script>
-  
-  <div class="h-auto mt-12 flex justify-center items-center text-base font-bold text-white">
-    <form class="flex flex-col justify-center items-center" on:submit|preventDefault={handleSubmit}>
-      <label for="passphrase">Enter Passphrase</label>
-  
-      <div class="flex bg-[#2E3654] px-3 mt-4 border rounded-3xl border-[#4C598B4D]">
-                {#if showPassword}
-                <input class="text-white bg-[#2E3654] border-0 tracking-wider font-normal border-transparent focus:border-transparent focus:ring-0" type="text" id="passphrase" bind:value={$passphrase} />
-            {:else}
-                <input class="text-white bg-[#2E3654] border-0 tracking-wider font-normal border-transparent focus:border-transparent focus:ring-0" type="password" id="passphrase" bind:value={$passphrase} />
-            {/if}
-            <button type="button" class="flex justify-center items-center" on:click={toggleShowPassword}>
-                <Eye />
-            </button>
-      </div>
-      {#if errorMessage}
-      <span class="text-xs text-red-500 font-thin">Passphrase doesn't match</span>
+  }
+</script>
+
+<div
+  class="h-auto mt-12 flex justify-center items-center text-base font-bold text-white"
+>
+  <form
+    class="flex flex-col justify-center items-center"
+    on:submit|preventDefault={handleSubmit}
+  >
+    <label for="passphrase">Enter Passphrase</label>
+
+    <div
+      class="flex bg-[#2E3654] px-3 mt-4 border rounded-3xl border-[#4C598B4D]"
+    >
+      {#if showPassword}
+        <input
+          class="text-white bg-[#2E3654] border-0 tracking-wider font-normal border-transparent focus:border-transparent focus:ring-0"
+          type="text"
+          id="passphrase"
+          bind:value={$passphrase}
+        />
+      {:else}
+        <input
+          class="text-white bg-[#2E3654] border-0 tracking-wider font-normal border-transparent focus:border-transparent focus:ring-0"
+          type="password"
+          id="passphrase"
+          bind:value={$passphrase}
+        />
       {/if}
       <button
         type="button"
