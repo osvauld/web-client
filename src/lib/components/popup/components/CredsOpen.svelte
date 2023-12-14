572df2af20235b08bec390da033954319fcb5657
tony <tonyantony300@gmail.com>
2023-12-14 18:03:24 +0530

    credential Injection done

--- src/lib/components/popup/components/CredsOpen.svelte
+++ src/lib/components/popup/components/CredsOpen.svelte
@@ -1,4 +1,5 @@
 <script>
+  import browser from "webextension-polyfill";
   import { list } from "../../../store/ui.store";
   import { selectedCredential } from "../../../store/ui.store";
   import Copy from "../../../../lib/components/basic/copyIcon.svelte";
@@ -14,18 +15,24 @@
       copiedUsername = copiedPassword = false;
     }, 2000);
   }
+
+  const fillCredentials =  async () => {
+    console.log('Fiiling signal received');
+    await browser.runtime.sendMessage({ action: "fillingSignal", body:[$list[$selectedCredential]?.username, $list[$selectedCredential]?.password] });
+  }
+
 </script>
 
 <div
   class="flex flex-col justify-center items-center w-[80%] h-[300px] my-24 mx-auto bg-[#2B324D] border rounded-md border-[#4C598B4D] text-white"
 >
   <div class="flex justify-end items-center w-3/4">
-    {$list[$selectedCredential].username}
+    {$list[$selectedCredential]?.username}
     {#if !copiedUsername}
       <span
         class="mx-4 cursor-pointer transition ease-in-out duration-500 transform hover:scale-110"
         on:click={() => {
-          copyOperation($list[$selectedCredential].username, false);
+          copyOperation($list[$selectedCredential]?.username, false);
         }}
       >
         <Copy /></span
@@ -37,12 +44,12 @@
     {/if}
   </div>
   <div class="flex justify-end items-center w-3/4">
-    {$list[$selectedCredential].password}
+    {$list[$selectedCredential]?.password}
     {#if !copiedPassword}
       <span
         class="mx-4 cursor-pointer transition ease-in-out duration-500 transform hover:scale-110"
         on:click={() => {
-          copyOperation($list[$selectedCredential].password, true);
+          copyOperation($list[$selectedCredential]?.password, true);
         }}
       >
         <Copy /></span
@@ -52,5 +59,9 @@
         <Tick />
       </span>
     {/if}
+  </div >
+
+  <div class="flex mt-20 justify-center items-center w-3/4 h-[100px]">
+    <button class="bg-[#4E46DC] py-3 px-12 rounded-3xl cursor-pointer active:scale-95" on:click={fillCredentials}>Fill</button>
   </div>
 </div>
