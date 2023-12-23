6d0db19b8dd130329d3f6a5387d205b457a16160
tony <tonyantony300@gmail.com>
2023-12-23 22:01:22 +0530

    types corrected

--- src/lib/components/popup/components/CredsListed.svelte
+++ src/lib/components/popup/components/CredsListed.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import Key from "../../../components/basic/key.svelte";
   import RightArrow from "../../../components/basic/rightArrow.svelte";
   import { list } from "../../../store/ui.store";
@@ -10,7 +10,7 @@
 
   // Need the code to fetch the Url from the background script and display it here.
 
-  function credentialClicked(index) {
+  function credentialClicked(index: any) {
     selectedCredential.set(index);
     credOpen.set(true);
   }
@@ -38,7 +38,7 @@
       {#each $list as item, index (item.username)}
         <button
           class="w-full h-[50px] border rounded-md border-[#4C598B4D] flex justify-between items-center px-5 my-1 cursor-pointer active:bg-[#353E60] active:scale-[0.98]"
-          on:click={() => credentialClicked(index)}
+          on:click={() => index && credentialClicked(index) }
         >
           <Key />
           <span>{item.username}</span>
