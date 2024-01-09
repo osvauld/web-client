5c785b39d7e05520c64cc294e0894ec3fa465b0b
abrahamgeorge8547 <abrahamgeorge8547@gmail.com>
2024-01-09 15:28:26 +0530

    full screen from popup

--- src/lib/components/popup/Home.svelte
+++ src/lib/components/popup/Home.svelte
@@ -2,9 +2,19 @@
   import CredsListed from "./components/CredsListed.svelte";
   import CredsOpen from "./components/CredsOpen.svelte";
   import { credOpen } from "../../store/ui.store";
+  import browser from "webextension-polyfill";
+
+  const openFullscreenTab = async () => {
+    // Send a message to the background sdaash
+    console.log("Opening fullscreen tab");
+    await browser.runtime.sendMessage({ action: "openFullscreenTab" });
+  };
 </script>
 
 <div class="flex justify-center flex-col items-center">
+  <button class="bg-red-400 p-4" on:click={openFullscreenTab}
+    >Open full screen</button
+  >
   <div class="h-[490px] w-11/12 bg-[#2E3654] my-3 overflow-hidden">
     {#if $credOpen}
       <CredsOpen />
