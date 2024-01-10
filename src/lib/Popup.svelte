cfaa6dac2286b1f4c8629d069e089cf3564141da
tony <tonyantony300@gmail.com>
2024-01-10 15:34:59 +0530

    changes 3

--- src/lib/Popup.svelte
+++ src/lib/Popup.svelte
@@ -6,7 +6,7 @@
   import Logo from "./components/basic/logo.svelte";
   import { isLoggedIn } from "../lib/store/ui.store";
   console.log("is logged in status", $isLoggedIn);
-  let devType = "sda";
+  let devType = "s";
 
   onMount(async () => {
     if (devType != "popup") openFullscreenTab();
