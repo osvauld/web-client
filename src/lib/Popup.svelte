c45b10ec3e81501a347812c88bc8d36a1dee5e29
tony <tonyantony300@gmail.com>
2024-01-18 13:32:06 +0530

    merge conflic resloution 1

--- src/lib/Popup.svelte
+++ src/lib/Popup.svelte
@@ -6,7 +6,7 @@
   import Logo from "./components/basic/logo.svelte";
   import { isLoggedIn, isSignedUp } from "../lib/store/ui.store";
   import Signup from "./components/popup/Signup.svelte";
-  let devType = "popup";
+  let devType = "popu";
 
   onMount(async () => {
     if (devType != "popup") openFullscreenTab();
