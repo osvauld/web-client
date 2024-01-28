16c35f68d76694f10fdb22a33d2031300aabeefa
tony <tonyantony300@gmail.com>
2024-01-29 00:13:17 +0530

    card inpus changed, few easy wins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,32 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:svelte/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    project: "./tsconfig.app.json",
+    ecmaVersion: "latest",
+    sourceType: "module",
+    extraFileExtensions: [".svelte"],
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        project: "./tsconfig.app.json",
+      },
     },
-    extends: ['eslint:recommended', 'plugin:svelte/recommended', ],
-    parser: '@typescript-eslint/parser',
-    plugins: ["@typescript-eslint"],
-    parserOptions: {
-        project: './tsconfig.app.json',
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        extraFileExtensions: ['.svelte']
+    {
+      files: ["src/**/*.ts"],
+      parserOptions: {
+        project: "./tsconfig.app.json",
+      },
     },
-    overrides: [
-        
-        {
-            files: ['*.svelte'],
-            parser: 'svelte-eslint-parser',
-            parserOptions: {
-              parser: '@typescript-eslint/parser',
-              project: './tsconfig.app.json',
-            }
-          },
-          {
-             files: ['src/**/*.ts' ],
-             parserOptions: {
-                project: './tsconfig.app.json',
-             }
-          }
-    ],
-    rules: {
-       
-    },
+  ],
+  rules: {},
 };
---
+++ src/lib/components/dashboard/credentials/PlainField.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { ActiveCopy, CopyIcon } from "../icons";
+    export let fieldName;
+    export let fieldValue;
+    export let hoverEffect;
+
+    const copyToClipboard = async () => {
+        try {
+            await navigator.clipboard.writeText(fieldValue);
+        } catch (err) {
+            console.error('Failed to copy: ', err);
+        }
+    };
+</script>
+
+<div class="mb-4 mr-1">
+    <label
+        class="label block mb-2 text-left text-osvauld-dusklabel text-sm font-normal cursor-pointer"
+        for="field">{fieldName}</label>
+    <div class="py-1 px-3 w-full flex justify-between items-center border rounded-lg text-base bg-osvauld-frameblack border-osvauld-iconblack {
+        hoverEffect
+            ? 'text-osvauld-quarzowhite'
+            : 'text-osvauld-sheffieldgrey'
+    }">
+        <span>{fieldValue}</span>
+        <button on:click={copyToClipboard}>
+            {#if hoverEffect}
+                <ActiveCopy />
+            {:else}
+                <CopyIcon />
+            {/if}
+        </button>
+    </div>
+</div>
