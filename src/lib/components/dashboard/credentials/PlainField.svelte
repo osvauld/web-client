d5a54b4fa3908c312daa88097d375e59463d1942
tony <tonyantony300@gmail.com>
2024-02-12 17:17:38 +0530

    long names hidden

--- src/lib/components/dashboard/credentials/PlainField.svelte
+++ src/lib/components/dashboard/credentials/PlainField.svelte
@@ -1,35 +1,35 @@
 <script>
-    import { ActiveCopy, CopyIcon } from "../icons";
-    export let fieldName;
-    export let fieldValue;
-    export let hoverEffect;
+  import { ActiveCopy, CopyIcon } from "../icons";
+  export let fieldName;
+  export let fieldValue;
+  export let hoverEffect;
 
-    const copyToClipboard = async () => {
-        try {
-            await navigator.clipboard.writeText(fieldValue);
-        } catch (err) {
-            console.error("Failed to copy: ", err);
-        }
-    };
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(fieldValue);
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  };
 </script>
 
-<div class="mb-4 mr-1">
-    <label
-        class="label block mb-2 text-left text-osvauld-dusklabel text-sm font-normal cursor-pointer"
-        for="field">{fieldName}</label
-    >
-    <div
-        class="py-1 px-3 w-full flex justify-between items-center border rounded-lg text-base bg-osvauld-bordergreen border-osvauld-iconblack {hoverEffect
-            ? 'text-osvauld-quarzowhite'
-            : 'text-osvauld-sheffieldgrey'}"
-    >
-        <span>{fieldValue}</span>
-        <button on:click={copyToClipboard}>
-            {#if hoverEffect}
-                <ActiveCopy />
-            {:else}
-                <CopyIcon />
-            {/if}
-        </button>
-    </div>
+<div class="mb-4 mr-1 max-w-full">
+  <label
+    class="label block mb-2 text-left text-osvauld-dusklabel text-sm font-normal cursor-pointer"
+    for="field">{fieldName}</label
+  >
+  <div
+    class="py-1 px-3 w-full flex justify-between items-center border rounded-lg text-base bg-osvauld-bordergreen border-osvauld-iconblack {hoverEffect
+      ? 'text-osvauld-quarzowhite'
+      : 'text-osvauld-sheffieldgrey'}"
+  >
+    <span class="max-w-[90%] overflow-x-hidden">{fieldValue}</span>
+    <button on:click={copyToClipboard}>
+      {#if hoverEffect}
+        <ActiveCopy />
+      {:else}
+        <CopyIcon />
+      {/if}
+    </button>
+  </div>
 </div>
--- src/lib/components/dashboard/folders/ShareFolderWithUsers.svelte
+++ src/lib/components/dashboard/folders/ShareFolderWithUsers.svelte
@@ -1,152 +1,152 @@
 <script lang="ts">
-    import { shareFolderWithUsers, fetchFolderUsers } from "../apis";
-    import {
-        User,
-        UserWithAccessType,
-        ShareFolderWithUsersPayload,
-        CredentialFields,
-    } from "../dtos";
-    import { selectedFolder, showFolderShareDrawer } from "../store";
-    import { createShareCredsPayload, setbackground } from "../helper";
-    import { Lens, DownArrow, RightArrow } from "../icons";
-    import ListItem from "../components/ListItem.svelte";
-    import ShareToast from "../components/ShareToast.svelte";
-    import ExistingListParent from "../components/ExistingListParent.svelte";
+  import { shareFolderWithUsers, fetchFolderUsers } from "../apis";
+  import {
+    User,
+    UserWithAccessType,
+    ShareFolderWithUsersPayload,
+    CredentialFields,
+  } from "../dtos";
+  import { selectedFolder, showFolderShareDrawer } from "../store";
+  import { createShareCredsPayload, setbackground } from "../helper";
+  import { Lens, DownArrow, RightArrow } from "../icons";
+  import ListItem from "../components/ListItem.svelte";
+  import ShareToast from "../components/ShareToast.svelte";
+  import ExistingListParent from "../components/ExistingListParent.svelte";
 
-    export let users: User[];
-    export let credentialsFields: CredentialFields[];
-    let selectedUsers: UserWithAccessType[] = [];
-    let showOptions = false;
-    let selectionIndex: number | null = null;
-    let topList = false;
-    let searchInput = "";
-    let existingItemDropdown = false;
-    let shareToast = false;
-    let existingUserData: UserWithAccessType[] = [];
+  export let users: User[];
+  export let credentialsFields: CredentialFields[];
+  let selectedUsers: UserWithAccessType[] = [];
+  let showOptions = false;
+  let selectionIndex: number | null = null;
+  let topList = false;
+  let searchInput = "";
+  let existingItemDropdown = false;
+  let shareToast = false;
+  let existingUserData: UserWithAccessType[] = [];
 
-    $: filteredUsers = searchInput
-        ? users.filter((user) =>
-              user.name.toLowerCase().includes(searchInput.toLowerCase()),
-          )
-        : users;
+  $: filteredUsers = searchInput
+    ? users.filter((user) =>
+        user.name.toLowerCase().includes(searchInput.toLowerCase())
+      )
+    : users;
 
-    const existingUsers = async () => {
-        existingItemDropdown = !existingItemDropdown;
-        if (existingUserData.length === 0 && $selectedFolder !== null) {
-            const responseJson = await fetchFolderUsers($selectedFolder.id);
-            existingUserData = responseJson.data;
-        } else {
-            existingUserData.length = 0;
-        }
+  const existingUsers = async () => {
+    existingItemDropdown = !existingItemDropdown;
+    if (existingUserData.length === 0 && $selectedFolder !== null) {
+      const responseJson = await fetchFolderUsers($selectedFolder.id);
+      existingUserData = responseJson.data;
+    } else {
+      existingUserData.length = 0;
+    }
+  };
+
+  const shareFolderHandler = async () => {
+    if ($selectedFolder === null) {
+      throw new Error("Folder not selected");
+    }
+    const userData = await createShareCredsPayload(
+      credentialsFields,
+      selectedUsers
+    );
+    const shareFolderPayload: ShareFolderWithUsersPayload = {
+      folderId: $selectedFolder.id,
+      // @ts-ignore
+      userData,
     };
+    const shareStatus = await shareFolderWithUsers(shareFolderPayload);
+    shareToast = shareStatus.success === true;
+  };
 
-    const shareFolderHandler = async () => {
-        if ($selectedFolder === null) {
-            throw new Error("Folder not selected");
-        }
-        const userData = await createShareCredsPayload(
-            credentialsFields,
-            selectedUsers,
-        );
-        const shareFolderPayload: ShareFolderWithUsersPayload = {
-            folderId: $selectedFolder.id,
-            // @ts-ignore
-            userData,
-        };
-        const shareStatus = await shareFolderWithUsers(shareFolderPayload);
-        shareToast = shareStatus.success === true;
-    };
+  function handleClick(index: number, isSelectedList: boolean) {
+    showOptions = !showOptions;
+    selectionIndex = index;
+    topList = isSelectedList;
+  }
 
-    function handleClick(index: number, isSelectedList: boolean) {
-        showOptions = !showOptions;
-        selectionIndex = index;
-        topList = isSelectedList;
+  function handleItemRemove(index: number) {
+    const removedUser = selectedUsers.splice(index, 1);
+    selectedUsers = [...selectedUsers];
+    const [{ accessType, ...userWithoutAccessType }] = removedUser;
+    users = [...users, { ...userWithoutAccessType }];
+  }
+
+  function handleRoleChange(e: any, index: number, type: string) {
+    const user = e.detail.item;
+    const option = e.detail.permission;
+    showOptions = !showOptions;
+    selectionIndex = null;
+    if (type === "selectedUsers") {
+      selectedUsers.splice(index, 1);
+      selectedUsers = [...selectedUsers, { ...user, accessType: option }];
+    } else {
+      selectedUsers = [...selectedUsers, { ...user, accessType: option }];
+      users = users.filter((u) => u.id !== user.id);
     }
-
-    function handleItemRemove(index: number) {
-        const removedUser = selectedUsers.splice(index, 1);
-        selectedUsers = [...selectedUsers];
-        const [{ accessType, ...userWithoutAccessType }] = removedUser;
-        users = [...users, { ...userWithoutAccessType }];
-    }
-
-    function handleRoleChange(e: any, index: number, type: string) {
-        const user = e.detail.item;
-        const option = e.detail.permission;
-        showOptions = !showOptions;
-        selectionIndex = null;
-        if (type === "selectedUsers") {
-            selectedUsers.splice(index, 1);
-            selectedUsers = [...selectedUsers, { ...user, accessType: option }];
-        } else {
-            selectedUsers = [...selectedUsers, { ...user, accessType: option }];
-            users = users.filter((u) => u.id !== user.id);
-        }
-    }
+  }
 </script>
 
 <div class="p-2 border border-osvauld-bordergreen rounded-lg h-[50vh]">
-    <div
-        class="h-[1.875rem] w-full px-2 mx-auto flex justify-start items-center border border-osvauld-bordergreen rounded-lg cursor-pointer mb-2"
+  <div
+    class="h-[1.875rem] w-full px-2 mx-auto flex justify-start items-center border border-osvauld-bordergreen rounded-lg cursor-pointer mb-2"
+  >
+    <Lens />
+    <input
+      type="text"
+      bind:value={searchInput}
+      class="h-[1.75rem] w-full bg-osvauld-frameblack border-0 text-osvauld-quarzowhite placeholder-osvauld-placeholderblack border-transparent text-base focus:border-transparent focus:ring-0 cursor-pointer"
+      placeholder="Search for users"
+    />
+  </div>
+
+  <div class="border border-osvauld-bordergreen my-1 w-full mb-1"></div>
+
+  <div
+    class="overflow-y-auto scrollbar-thin h-[35vh] bg-osvauld-frameblack w-full"
+  >
+    {#each selectedUsers as user, index}
+      <ListItem
+        item={user}
+        isSelected={index === selectionIndex && topList}
+        isTopList={true}
+        on:click={() => handleClick(index, true)}
+        on:remove={() => handleItemRemove(index)}
+        {setbackground}
+        {showOptions}
+        on:select={(e) => handleRoleChange(e, index, "selectedUsers")}
+      />
+    {/each}
+    {#each filteredUsers as user, index}
+      <ListItem
+        item={user}
+        isSelected={index === selectionIndex && !topList}
+        isTopList={false}
+        on:click={() => handleClick(index, false)}
+        {setbackground}
+        {showOptions}
+        on:select={(e) => handleRoleChange(e, index, "users")}
+      />
+    {/each}
+  </div>
+
+  <div class="p-2 flex justify-between items-center box-border">
+    <button
+      class="w-[45%] px-4 py-2 bg-osvauld-iconblack border border-osvauld-placeholderblack rounded-md text-osvauld-sheffieldgrey"
+      >Cancel</button
     >
-        <Lens />
-        <input
-            type="text"
-            bind:value={searchInput}
-            class="h-[1.75rem] w-full bg-osvauld-frameblack border-0 text-osvauld-quarzowhite placeholder-osvauld-placeholderblack border-transparent text-base focus:border-transparent focus:ring-0 cursor-pointer"
-            placeholder="Search for users"
-        />
-    </div>
 
-    <div class="border border-osvauld-bordergreen my-1 w-full mb-1"></div>
-
-    <div
-        class="overflow-y-auto scrollbar-thin h-[35vh] bg-osvauld-frameblack w-full"
+    <button
+      class="w-[45%] px-4 py-2 bg-osvauld-carolinablue text-macchiato-surface0 rounded-md"
+      on:click={shareFolderHandler}>Share</button
     >
-        {#each selectedUsers as user, index}
-            <ListItem
-                item={user}
-                isSelected={index === selectionIndex && topList}
-                isTopList={true}
-                on:click={() => handleClick(index, true)}
-                on:remove={() => handleItemRemove(index)}
-                {setbackground}
-                {showOptions}
-                on:select={(e) => handleRoleChange(e, index, "selectedUsers")}
-            />
-        {/each}
-        {#each filteredUsers as user, index}
-            <ListItem
-                item={user}
-                isSelected={index === selectionIndex && !topList}
-                isTopList={false}
-                on:click={() => handleClick(index, false)}
-                {setbackground}
-                {showOptions}
-                on:select={(e) => handleRoleChange(e, index, "users")}
-            />
-        {/each}
-    </div>
-
-    <div class="p-2 flex justify-between items-center box-border">
-        <button
-            class="w-[45%] px-4 py-2 bg-osvauld-iconblack border border-osvauld-placeholderblack rounded-md text-osvauld-sheffieldgrey"
-            >Cancel</button
-        >
-
-        <button
-            class="w-[45%] px-4 py-2 bg-osvauld-carolinablue text-macchiato-surface0 rounded-md"
-            on:click={shareFolderHandler}>Share</button
-        >
-    </div>
-    {#if shareToast}
-        <ShareToast on:close={() => (shareToast = !shareToast)} />
-    {/if}
+  </div>
+  {#if shareToast}
+    <ShareToast on:close={() => (shareToast = !shareToast)} />
+  {/if}
 </div>
 
 <ExistingListParent
-    {existingItemDropdown}
-    existingItemsData={existingUserData}
-    user={true}
-    on:click={existingUsers}
+  {existingItemDropdown}
+  existingItemsData={existingUserData}
+  user={true}
+  on:click={existingUsers}
 />
