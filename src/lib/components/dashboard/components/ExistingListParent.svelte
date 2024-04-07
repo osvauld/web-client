085ab0cbc2a614317191fa55f2b9e32b8fad09a5
abrahamgeorge8547 <abrahamgeorge8547@gmail.com>
2024-04-07 22:42:05 +0530

    folder access edit fixed

--- src/lib/components/dashboard/components/ExistingListParent.svelte
+++ src/lib/components/dashboard/components/ExistingListParent.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   import { createEventDispatcher } from "svelte";
   import ExistingListItem from "./ExistingListItem.svelte";
-  import { DownArrow, RightArrow } from "../icons";
+  import { DownArrow, RightArrow, EditIcon } from "../icons";
 
   export let existingItemDropdown;
   export let existingItemsData;
   export let user;
-
+  let editPermissionTrigger = false;
   const dispatch = createEventDispatcher();
 
   const dispatchClick = () => {
@@ -44,11 +44,22 @@
     class="overflow-y-scroll scrollbar-thin min-h-0 max-h-[18vh] bg-osvauld-frameblack w-full"
   >
     {#if existingItemDropdown}
+      <button
+        class="p-2 rounded-lg {editPermissionTrigger
+          ? 'bg-osvauld-sensitivebgblue'
+          : ''}"
+        on:click={() => {
+          editPermissionTrigger = !editPermissionTrigger;
+        }}
+      >
+        <EditIcon />
+      </button>
       {#each existingItemsData as item, index}
         <!-- TODO: user should not be able to remove themselves -->
         <ExistingListItem
           {index}
           {item}
+          {editPermissionTrigger}
           on:remove={() => handleRemoval(item)}
           on:permissonChange={(e) => handlePermissionChange(e, item)}
         />
