e42cae9f1215500a7674836ff7d04842d7d4c234
tony <tonyantony300@gmail.com>
2024-04-11 12:16:59 +0530

    success toast ok

--- src/lib/components/dashboard/components/ShareToast.svelte
+++ src/lib/components/dashboard/components/ShareToast.svelte
@@ -2,29 +2,31 @@
   import { onMount } from "svelte";
   import { BlueClose } from "../icons";
   import { toastStore } from "../store";
-
+  let closehovered = false;
   const triggerCloseEvent = () => {
     toastStore.set({ show: false, message: "", type: null });
   };
-  // onMount(() => {
-  //   setTimeout(() => {
-  //     triggerCloseEvent();
-  //   }, 1000);
-  // });
+  onMount(() => {
+    setTimeout(() => {
+      triggerCloseEvent();
+    }, 2000);
+  });
 </script>
 
 <div
-  class="fixed inset-0 flex items-center justify-center z-500 px-4 py-5 my-2 rounded-lg bg-osvauld-bordergreen text-osvauld-quarzowhite font-normal text-xl"
+  class="fixed inset-0 flex items-end justify-end px-4 py-5 my-2 rounded-lg bg-transparent font-normal text-xl !z-[1000] mr-4 mb-4"
 >
   <div
-    class="justify-between items-center rounded-lg bg-osvauld-bordergreen text-osvauld-quarzowhite font-normal text-xl px-4 py-5 my-2"
+    class="justify-between items-center rounded-xl text-osvauld-chalkwhite border border-osvauld-iconblack font-normal text-xl pl-4 pr-2 py-5 my-2 bg-osvauld-cardshade"
   >
     <span class="ml-2 text-base font-sans"> {$toastStore.message}!</span>
     <button
-      class="border-0 bg-osvauld-toastCloseBlue rounded-lg justify-center items-center p-2 mr-5"
+      class="border-0 hover:bg-osvauld-modalFieldActive rounded-lg justify-center items-center p-2 mx-3"
       on:click={triggerCloseEvent}
+      on:mouseenter={() => (closehovered = true)}
+      on:mouseleave={() => (closehovered = false)}
     >
-      <BlueClose /></button
+      <BlueClose color={closehovered ? "#F2F2F0" : "#67697C"} /></button
     >
   </div>
 </div>
