4345d069ba60d0ec78543f43f3e287464add2523
tony <tonyantony300@gmail.com>
2024-05-03 19:33:33 +0530

    conflict resolution

--- src/lib/components/popup/AddCredential.svelte
+++ src/lib/components/popup/AddCredential.svelte
@@ -1,156 +1,166 @@
 <script lang="ts">
-    import browser from "webextension-polyfill";
-    import {
-        addCredential,
-        fetchAllFolders,
-        fetchFolderUsersForDataSync,
-    } from "../dashboard/apis";
-    import { sendMessage } from "../dashboard/helper";
-    export let username = "";
-    export let password = "";
-    export let domain = "";
-    export let windowId;
-    let name = "";
-    let description = "";
-    let showFolderList = false;
-    let folderData = [];
+  import browser from "webextension-polyfill";
+  import {
+    addCredential,
+    fetchAllFolders,
+    fetchFolderUsersForDataSync,
+  } from "../dashboard/apis";
+  import { sendMessage } from "../dashboard/helper";
+  import { spawn } from "child_process";
+  export let username = "";
+  export let password = "";
+  export let domain = "";
+  export let windowId;
+  let name = "";
+  let description = "";
+  let showFolderList = false;
+  let folderData = [];
 
-    let addCredentialPayload = {
-        name: name,
-        folderId: "",
-        description: description,
-        credentialType: "Login",
-        userFields: [],
-        domain,
-    };
-    // Function to handle form submission
-    const handleSubmit = async () => {
-        const credential = { username, password, domain, name, description };
+  let addCredentialPayload = {
+    name: name,
+    folderId: "",
+    description: description,
+    credentialType: "Login",
+    userFields: [],
+    domain,
+  };
+  // Function to handle form submission
+  const handleSubmit = async () => {
+    const credential = { username, password, domain, name, description };
 
-        const responseJson = await fetchAllFolders();
-        folderData = responseJson.data.sort((a, b) =>
-            a.name.localeCompare(b.name),
-        );
-        showFolderList = true;
-        console.log("Submitting credential:", credential);
+    const responseJson = await fetchAllFolders();
+    folderData = responseJson.data.sort((a, b) => a.name.localeCompare(b.name));
+    showFolderList = true;
+    console.log("Submitting credential:", credential);
 
-        // Implement actual data handling here, such as an API call
-    };
+    // Implement actual data handling here, such as an API call
+  };
 
-    const handleFolderSelect = async (folderId) => {
-        console.log("Selected folder:", folderId);
-        const response = await fetchFolderUsersForDataSync(folderId);
-        const usersToShare = response.data;
-        const fieldPayload = [
-            { fieldName: "Username", fieldValue: username, fieldType: "meta" },
-            {
-                fieldName: "Password",
-                fieldValue: password,
-                fieldType: "sensitive",
-            },
-            {
-                fieldName: "Domain",
-                fieldValue: domain,
-                fieldType: "additional",
-            },
-            { fieldName: "URL", fieldValue: domain, fieldType: "meta" },
-        ];
-        addCredentialPayload.folderId = folderId;
-        const userFields = await sendMessage("addCredential", {
-            users: usersToShare,
-            addCredentialFields: fieldPayload,
-        });
-        addCredentialPayload.name = name;
-        addCredentialPayload.description = description;
-        addCredentialPayload.userFields = userFields;
-        console.log(addCredentialPayload);
-        await addCredential(addCredentialPayload);
-        showFolderList = false;
-        await browser.windows.remove(windowId);
-        // showFolderList = false;
-    };
+  const handleFolderSelect = async (folderId) => {
+    console.log("Selected folder:", folderId);
+    const response = await fetchFolderUsersForDataSync(folderId);
+    const usersToShare = response.data;
+    const fieldPayload = [
+      { fieldName: "Username", fieldValue: username, fieldType: "meta" },
+      {
+        fieldName: "Password",
+        fieldValue: password,
+        fieldType: "sensitive",
+      },
+      {
+        fieldName: "Domain",
+        fieldValue: domain,
+        fieldType: "additional",
+      },
+      { fieldName: "URL", fieldValue: domain, fieldType: "meta" },
+    ];
+    addCredentialPayload.folderId = folderId;
+    const userFields = await sendMessage("addCredential", {
+      users: usersToShare,
+      addCredentialFields: fieldPayload,
+    });
+    addCredentialPayload.name = name;
+    addCredentialPayload.description = description;
+    addCredentialPayload.userFields = userFields;
+    console.log(addCredentialPayload);
+    await addCredential(addCredentialPayload);
+    showFolderList = false;
+    await browser.windows.remove(windowId);
+    // showFolderList = false;
+  };
 </script>
 
 {#if showFolderList}
-    folderData
-    <ul>
-        {#each folderData as folder}
-            <li>
-                <button on:click={() => handleFolderSelect(folder.id)}>
-                    <div class="p-2 mb-2 bg-white">{folder.name}</div>
-                </button>
-            </li>
-        {/each}
-    </ul>
-{:else}
-    <form
-        on:submit|preventDefault={handleSubmit}
-        class="flex flex-col p-4 max-w-sm mx-auto space-y-4"
-    >
-        <div>
-            <label
-                for="username"
-                class="block text-sm font-medium text-gray-700">Username</label
-            >
-            <input
-                id="username"
-                type="text"
-                bind:value={username}
-                class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-        </div>
-        <div>
-            <label
-                for="password"
-                class="block text-sm font-medium text-gray-700">Password</label
-            >
-            <input
-                id="password"
-                type="password"
-                bind:value={password}
-                class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-        </div>
-        <div>
-            <label for="domain" class="block text-sm font-medium text-gray-700"
-                >Domain</label
-            >
-            <input
-                id="domain"
-                type="text"
-                bind:value={domain}
-                class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-        </div>
-        <div>
-            <label for="name" class="block text-sm font-medium text-gray-700"
-                >Name</label
-            >
-            <input
-                id="name"
-                type="text"
-                bind:value={name}
-                class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-        </div>
-        <div>
-            <label
-                for="description"
-                class="block text-sm font-medium text-gray-700"
-                >Description</label
-            >
-            <textarea
-                id="description"
-                bind:value={description}
-                rows="3"
-                class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            ></textarea>
-        </div>
-        <button
-            type="submit"
-            class="px-4 py-2 bg-indigo-600 text-white font-bold rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
-        >
-            Next
+  <div class="text-osvauld-textActive text-base max-w-full pl-4">
+    Select Folder to add this credential
+  </div>
+  <ul>
+    {#each folderData as folder}
+      <li>
+        <button on:click={() => handleFolderSelect(folder.id)}>
+          <div class="p-2 mb-2 bg-white">{folder.name}</div>
         </button>
-    </form>
+      </li>
+    {/each}
+  </ul>
+{:else}
+  <div class="text-osvauld-textActive text-base max-w-full pl-4">
+    Do you want to add this credential to osvauld?
+  </div>
+  <form
+    on:submit|preventDefault={handleSubmit}
+    class="flex flex-col p-4 max-w-sm mx-auto space-y-2 text-osvauld-textActive"
+  >
+    <div>
+      <input
+        id="name"
+        type="text"
+        placeholder="Enter credential name"
+        bind:value={name}
+        class="mt-1 block w-full px-3 py-1 rounded-md shadow-sm sm:text-sm bg-osvauld-frameblack border-osvauld-iconblack focus:border-osvauld-iconblack focus:ring-0"
+        autocomplete="off"
+      />
+    </div>
+    <div>
+      <label
+        for="username"
+        class="block text-sm font-medium text-osvauld-textActive"
+        >Username</label
+      >
+      <input
+        id="username"
+        type="text"
+        bind:value={username}
+        autocomplete="off"
+        class="mt-1 block w-full px-3 py-1 shadow-sm sm:text-sm text-osvauld-fieldTextActive bg-osvauld-fieldActive rounded-md border-0 focus:border-osvauld-iconblack focus:ring-0"
+      />
+    </div>
+    <div>
+      <label
+        for="password"
+        class="block text-sm font-medium text-osvauld-textActive"
+        >Password</label
+      >
+      <input
+        id="password"
+        type="password"
+        bind:value={password}
+        autocomplete="off"
+        class="mt-1 block w-full px-3 py-1 shadow-sm sm:text-sm text-osvauld-fieldTextActive bg-osvauld-fieldActive rounded-md border-0 focus:border-osvauld-iconblack focus:ring-0"
+      />
+    </div>
+    <div>
+      <label
+        for="domain"
+        class="block text-sm font-medium text-osvauld-textActive">Domain</label
+      >
+      <input
+        id="domain"
+        type="text"
+        bind:value={domain}
+        autocomplete="off"
+        class="mt-1 block w-full px-3 py-1 shadow-sm sm:text-sm text-osvauld-fieldTextActive bg-osvauld-fieldActive rounded-md border-0 focus:border-osvauld-iconblack focus:ring-0"
+      />
+    </div>
+
+    <div>
+      <label
+        for="description"
+        class="block text-sm font-medium text-osvauld-textActive"
+        >Description</label
+      >
+      <textarea
+        id="description"
+        bind:value={description}
+        rows="3"
+        class="mt-1 block w-full px-3 py-1 shadow-sm sm:text-sm text-osvauld-fieldTextActive bg-osvauld-frameblack rounded-md border border-osvauld-iconblack focus:border-osvauld-iconblack focus:ring-0"
+      ></textarea>
+    </div>
+    <button
+      type="submit"
+      class="px-4 py-2 bg-osvauld-carolinablue text-osvauld-frameblack font-normal rounded-md active::scale-95"
+    >
+      Next
+    </button>
+  </form>
 {/if}
--- src/lib/components/popup/Home.svelte
+++ src/lib/components/popup/Home.svelte
@@ -26,6 +26,14 @@
   let scrollPosition = 0;
   let clickedCredential: any | null = null;
   let scrollableElement;
+  let port: browser.Runtime.Port;
+  let addNewCredential = true;
+  let newCredential: any | null = {
+    username: "",
+    password: "",
+    domain: "",
+    windowId: "",
+  };
   const openFullscreenTab = async () => {
     await sendMessage("openFullscreenTab");
   };
@@ -133,55 +141,60 @@
 </script>
 
 <div class="w-full h-full px-2">
-  <div class="flex justify-between items-center mb-3 py-0">
-    <h6 class="text-2xl font-medium text-osvauld-fieldText tracking-wide">
-      osvauld
-    </h6>
-    <div>
-      <button class="" on:click={openFullscreenTab}>
-        <Maximize />
-      </button>
+  {#if !addNewCredential}
+    <div class="flex justify-between items-center mb-3 py-0">
+      <h6 class="text-2xl font-medium text-osvauld-fieldText tracking-wide">
+        osvauld
+      </h6>
+      <div>
+        <button class="" on:click={openFullscreenTab}>
+          <Maximize />
+        </button>
+      </div>
     </div>
-  </div>
+  {/if}
 
   <div class="w-full h-[90%] overflow-hidden">
-    <div
-      class="text-osvauld-highlightwhite mb-3 flex justify-between items-center text-sm"
-    >
-      <span class="text-base text-osvauld-carolinablue">
-        {#if domain}
-          {domain}
-        {/if}
-      </span>
-      <span
-        class="text-osvauld-sheffieldgrey {passwordFound
-          ? 'visible'
-          : 'invisible'}"
+    {#if !addNewCredential}
+      <div
+        class="text-osvauld-highlightwhite mb-3 flex justify-between items-center text-sm"
       >
-        {domainAssociatedCredentials.length}
-      </span>
-    </div>
+        <span class="text-base text-osvauld-carolinablue">
+          {#if domain}
+            {domain}
+          {/if}
+        </span>
+        <span
+          class="text-osvauld-sheffieldgrey {passwordFound
+            ? 'visible'
+            : 'invisible'}"
+        >
+          {domainAssociatedCredentials.length}
+        </span>
+      </div>
 
-    <div
-      class="h-9 w-full mx-auto flex justify-start items-center border focus-within:!border-osvauld-activeBorder border-osvauld-iconblack rounded-lg cursor-pointer mb-4 pl-2"
-    >
-      <Lens />
-      <input
-        type="text"
-        class="h-6 w-[70%] bg-osvauld-frameblack border-0 text-osvauld-quarzowhite placeholder-osvauld-placeholderblack border-transparent text-sm font-light focus:border-transparent focus:ring-0 cursor-pointer"
-        placeholder="Find what you need faster.."
-        on:keyup={handleInputChange}
-        bind:value={query}
-        autofocus
-      />
-    </div>
-
+      <div
+        class="h-9 w-full mx-auto flex justify-start items-center border focus-within:!border-osvauld-activeBorder border-osvauld-iconblack rounded-lg cursor-pointer mb-4 pl-2"
+      >
+        <Lens />
+        <input
+          type="text"
+          class="h-6 w-[70%] bg-osvauld-frameblack border-0 text-osvauld-quarzowhite placeholder-osvauld-placeholderblack border-transparent text-sm font-light focus:border-transparent focus:ring-0 cursor-pointer"
+          placeholder="Find what you need faster.."
+          on:keyup={handleInputChange}
+          bind:value={query}
+          autofocus
+        />
+      </div>
+    {/if}
     <div class="h-full p-0 scrollbar-thin">
+      {#if !addNewCredential}
+        <div
+          class="border-b border-osvauld-darkLineSeperator mb-1 w-[calc(100%+1.5rem)] -translate-x-3"
+        ></div>
+      {/if}
       <div
-        class="border-b border-osvauld-darkLineSeperator mb-1 w-[calc(100%+1.5rem)] -translate-x-3"
-      ></div>
-      <div
-        class="h-[25rem] overflow-y-scroll scrollbar-thin pt-3"
+        class="min-h-[25rem] overflow-y-scroll scrollbar-thin pt-3"
         on:scroll={handleScroll}
         bind:this={scrollableElement}
       >
