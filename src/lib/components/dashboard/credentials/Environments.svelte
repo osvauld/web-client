6d29fc585870f10eb9fd8ddfd7c6ba1588e7a2eb
tony <tonyantony300@gmail.com>
2024-05-16 14:06:27 +0530

    environment buttons fixed

--- src/lib/components/dashboard/credentials/Environments.svelte
+++ src/lib/components/dashboard/credentials/Environments.svelte
@@ -1,92 +1,114 @@
 <script lang="ts">
-    import {
-        selectedEnv,
-        showAddCliDrawer,
-        envStore,
-        showAddEnvDrawer,
-    } from "../store";
-    import AddCredentialToEnv from "./AddCredentialToEnv.svelte";
-    import AddCliUser from "./AddCliUser.svelte";
-    import AddEnvironment from "./AddEnvironment.svelte";
-    import { Add } from "../icons";
-    import { fetchEnvFields } from "../apis";
+  import {
+    selectedEnv,
+    showAddCliDrawer,
+    envStore,
+    showAddEnvDrawer,
+  } from "../store";
+  import AddCredentialToEnv from "./AddCredentialToEnv.svelte";
+  import AddCliUser from "./AddCliUser.svelte";
+  import { Add } from "../icons";
+  import { fetchEnvFields } from "../apis";
+  import EnvironmentAdd from "../../basic/icons/environmentAdd.svelte";
+  import UserPlus from "../../basic/icons/userPlus.svelte";
+  import AddEnvironment from "./AddEnvironment.svelte";
 
-    let addCredentialHovered = false;
-    let addcredentialToEnv = false;
-    let fields = [];
-    const addEnv = () => {
-        showAddEnvDrawer.set(true);
-    };
+  let addCredentialHovered = false;
+  let addcredentialToEnv = false;
+  let addNewUserHovered = false;
+  let addNewCliUserHovered = false;
+  let fields = [];
+  const addEnv = () => {
+    showAddEnvDrawer.set(true);
+  };
 
-    const addCliUser = () => {
-        showAddCliDrawer.set(true);
-    };
-    selectedEnv.subscribe(async (value) => {
-        if (value) {
-            const fieldsResponse = await fetchEnvFields(value.id);
-            fields = fieldsResponse.data;
-        }
-    });
+  const addCliUser = () => {
+    showAddCliDrawer.set(true);
+  };
+  selectedEnv.subscribe(async (value) => {
+    if (value) {
+      const fieldsResponse = await fetchEnvFields(value.id);
+      fields = fieldsResponse.data;
+    }
+  });
 </script>
 
-<div class="flex justify-between">
-    <h1
-        class="text-3xl p-4 font-normal whitespace-nowrap text-osvauld-sideListTextActive"
-    >
-        {#if $selectedEnv}
-            {$selectedEnv.name}
-        {:else}
-            Environments
-        {/if}
-    </h1>
+<div class="flex justify-between items-center my-4 p-2">
+  <h1
+    class="text-3xl p-4 font-normal whitespace-nowrap text-osvauld-sideListTextActive"
+  >
     {#if $selectedEnv}
-        <button
-            class="rounded-md py-1.5 px-4 mx-2 flex justify-center items-center whitespace-nowrap text-sm border text-osvauld-textActive border-osvauld-iconblack hover:text-osvauld-frameblack hover:bg-osvauld-carolinablue"
-            on:mouseenter={() => (addCredentialHovered = true)}
-            on:mouseleave={() => (addCredentialHovered = false)}
-            on:click={() => (addcredentialToEnv = true)}
-        >
-            <span class="mr-2">Add Credential</span>
-            <Add color={addCredentialHovered ? "#0D0E13" : "#A3A4B5"} />
-        </button>
+      {$selectedEnv.name}
     {:else}
-        <button on:click={addCliUser}>create cli user</button>
-
-        <button on:click={addEnv}> add environment </button>
+      Environments
     {/if}
+  </h1>
+  {#if $selectedEnv}
+    <button
+      class="rounded-md py-1.5 px-4 mx-2 flex justify-center items-center whitespace-nowrap text-sm border text-osvauld-textActive border-osvauld-iconblack hover:text-osvauld-frameblack hover:bg-osvauld-carolinablue"
+      on:mouseenter={() => (addCredentialHovered = true)}
+      on:mouseleave={() => (addCredentialHovered = false)}
+      on:click={() => (addcredentialToEnv = true)}
+    >
+      <span class="mr-2">Add Credential</span>
+      <Add color={addCredentialHovered ? "#0D0E13" : "#A3A4B5"} />
+    </button>
+  {:else}
+    <div class="flex items-center gap-4">
+      <button
+        class="rounded-md h-[40px] py-1.5 px-4 mx-2 flex justify-center items-center whitespace-nowrap text-sm border text-osvauld-textActive border-osvauld-iconblack hover:text-osvauld-frameblack hover:bg-osvauld-carolinablue"
+        on:mouseenter={() => (addNewCliUserHovered = true)}
+        on:mouseleave={() => (addNewCliUserHovered = false)}
+        on:click={addCliUser}
+      >
+        <span class="mr-2">Add new CLI user</span>
+        <UserPlus color={addNewCliUserHovered ? "#0D0E13" : "#A3A4B5"} />
+      </button>
+
+      <button
+        class="rounded-md h-[40px] py-1.5 px-4 mx-2 flex justify-center items-center whitespace-nowrap text-sm border text-osvauld-textActive border-osvauld-iconblack hover:text-osvauld-frameblack hover:bg-osvauld-carolinablue"
+        on:mouseenter={() => (addNewUserHovered = true)}
+        on:mouseleave={() => (addNewUserHovered = false)}
+        on:click={addEnv}
+      >
+        <span class="mr-2">Add new environment </span>
+        <EnvironmentAdd color={addNewUserHovered ? "#0D0E13" : "#A3A4B5"} />
+      </button>
+    </div>
+  {/if}
 </div>
 
 {#if $selectedEnv}
-    <ul>
-        {#each fields as field}
-            <li>
-                {field.fieldName}
-                {field.fieldValue}
-            </li>
-        {/each}
-    </ul>
+  <ul>
+    {#each fields as field}
+      <li>
+        {field.fieldName}
+        {field.fieldValue}
+      </li>
+    {/each}
+  </ul>
 {:else}
-    <ul>
-        {#each $envStore as env}
-            <li>
-                {env.name}
-            </li>
-        {/each}
-    </ul>
+  <ul>
+    {#each $envStore as env}
+      <li>
+        {env.name}
+      </li>
+    {/each}
+  </ul>
 {/if}
 {#if addcredentialToEnv}
-    <button
-        class="fixed inset-0 flex items-center justify-center z-50 bg-osvauld-backgroundBlur backdrop-filter backdrop-blur-[2px]"
-        on:click={() => !addcredentialToEnv}
-    >
-        <button class="p-6 rounded bg-transparent" on:click|stopPropagation>
-            <AddCredentialToEnv on:close={() => (addcredentialToEnv = false)} />
-        </button>
+  <button
+    class="fixed inset-0 flex items-center justify-center z-50 bg-osvauld-backgroundBlur backdrop-filter backdrop-blur-[2px]"
+    on:click={() => !addcredentialToEnv}
+  >
+    <button class="p-6 rounded bg-transparent" on:click|stopPropagation>
+      <AddCredentialToEnv on:close={() => (addcredentialToEnv = false)} />
     </button>
+  </button>
 {/if}
 {#if $showAddCliDrawer}
-    <AddCliUser />
+  <AddCliUser />
 {/if}
 {#if $showAddEnvDrawer}
-    <AddEnvironment />
+  <AddEnvironment />
 {/if}
