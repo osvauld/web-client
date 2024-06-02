082cd28015cf1c4e559f83af3a85230864d6f636
tony <tonyantony300@gmail.com>
2024-06-02 14:48:21 +0530

    Suggested changes made

--- src/lib/components/popup/PasswordGenerator.svelte
+++ src/lib/components/popup/PasswordGenerator.svelte
@@ -15,14 +15,15 @@
 	let includeSpecialChars = false;
 	let includeNumbers = true;
 	let includeCapital = true;
-	let passwordLength = 14;
+	let passwordLength = 16;
 	let generatedPassword = "";
 	let entropy = null;
 	let copied = false;
 	let hoverEffect = false;
 	let strengthSticker: string = "";
 	let rotationDegree: number = 0;
-	const specialChars = "!#%+:=?@";
+
+	const specialChars = "!#$%&()*+,-.:;<=>?@[]^_{|}~";
 	const numbers = "0123456789";
 	const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 	const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -43,17 +44,19 @@
 		const entropy = calculateEntropy(password);
 		if (entropy < 40) {
 			strengthSticker = "Weak";
-		} else if (entropy < 130) {
+		} else if (entropy < 80) {
 			strengthSticker = "Fair";
-		} else if (entropy < 250) {
+		} else if (entropy < 120) {
 			strengthSticker = "Strong";
+		} else if (entropy < 160) {
+			strengthSticker = "Very Strong";
 		} else {
 			strengthSticker = "Chad";
 		}
-		console.log("Entropy:", entropy, "bits", "Strength:", strengthSticker);
 	}
 
 	function shuffleString(str: string): string {
+		// The Fisher-Yates (Knuth) shuffle algorithm
 		let array = str.split("");
 		for (let i = array.length - 1; i > 0; i--) {
 			const j = Math.floor(
@@ -64,12 +67,12 @@
 		return array.join("");
 	}
 
-	function generatePassword(
+	const generatePassword = (
 		passwordLength,
 		includeSpecialChars,
 		includeNumbers,
 		includeCapital,
-	) {
+	) => {
 		let allChars = lowerCase;
 		let mandatoryChars = "";
 		if (includeSpecialChars) {
@@ -100,7 +103,7 @@
 
 		generatedPassword = shuffleString(mandatoryChars + randomChars);
 		setStrengthSticker(generatedPassword);
-	}
+	};
 
 	$: {
 		generatePassword(
@@ -111,7 +114,7 @@
 		);
 	}
 
-	const copyToClipboard = async (e) => {
+	const copyToClipboard = async () => {
 		copied = true;
 		try {
 			await navigator.clipboard.writeText(generatedPassword);
@@ -147,21 +150,17 @@
 </style>
 
 <div
-	class="w-full h-full flex justify-center items-center text-osvauld-quarzowhite box-border p-2"
->
+	class="w-full h-full flex justify-center items-center text-osvauld-quarzowhite box-border p-2">
 	<div
-		class="w-full border border-osvauld-iconblack rounded-xl bg-osvauld-cardshade text-osvauld-fieldText overflow-hidden"
-	>
+		class="w-full border border-osvauld-iconblack rounded-xl bg-osvauld-cardshade text-osvauld-fieldText overflow-hidden">
 		<div
-			class="text-osvauld-fieldTextActive text-lg font-medium flex justify-center items-center flex-wrap min-h-[120px] relative"
-		>
+			class="text-osvauld-fieldTextActive text-lg font-medium flex justify-center items-center flex-wrap min-h-[120px] relative">
 			<span class="max-w-[80%] pt-3 pb-0 break-words">{generatedPassword}</span>
 			<div class="absolute bottom-1 right-10 flex gap-2">
 				<button
 					on:click|preventDefault|stopPropagation="{copyToClipboard}"
 					on:mouseenter="{() => (hoverEffect = true)}"
-					on:mouseleave="{() => (hoverEffect = false)}"
-				>
+					on:mouseleave="{() => (hoverEffect = false)}">
 					{#if copied}
 						<span in:scale>
 							<Tick />
@@ -175,8 +174,7 @@
 				<button
 					on:click|stopPropagation="{regenerationHandler}"
 					class="transition-transform duration-300 ease-in rotate-custom"
-					style="--rotation-deg: {rotationDegree}deg;"><Refresh /></button
-				>
+					style="--rotation-deg: {rotationDegree}deg;"><Refresh /></button>
 			</div>
 		</div>
 		<div class="border-b border-osvauld-iconblack w-[90%] translate-x-4"></div>
@@ -185,44 +183,39 @@
 			<div class="flex flex-col mt-5 items-center">
 				<div class="flex justify-between items-center gap-2">
 					<label class="w-[9rem]" for="lengthSlider"
-						>Length {passwordLength}</label
-					>
+						>Length {passwordLength}</label>
 					{#if strengthSticker === "Chad"}
 						<span class="flex justify-center items-center gap-1 w-[6rem]"
-							><Crown /> Chad</span
-						>
+							><Crown /> Chad</span>
+					{:else if strengthSticker === "Very Strong"}
+						<span class="flex justify-center items-center gap-1 w-[6rem]"
+							><ShieldCheck />Very Strong</span>
 					{:else if strengthSticker === "Strong"}
 						<span class="flex justify-center items-center gap-1 w-[6rem]"
-							><ShieldCheck />Very Strong</span
-						>
+							><ShieldCheck />Strong</span>
 					{:else if strengthSticker === "Fair"}
 						<span class="flex justify-center items-center w-[6rem]"
-							><CheckVerified /> Strong</span
-						>
+							><CheckVerified />Fair</span>
 					{:else}
 						<span class="flex justify-center items-center gap-1 w-[6rem]"
-							><AlertOctagon /> Weak</span
-						>
+							><AlertOctagon />Weak</span>
 					{/if}
 				</div>
 				<input
 					type="range"
 					id="lengthSlider"
-					min="4"
-					max="56"
+					min="8"
+					max="64"
 					bind:value="{passwordLength}"
-					class="slider w-[80%] my-3 mx-0 h-2 rounded-lg appearance-none cursor-pointer bg-osvauld-placeholderblack"
-				/>
+					class="slider w-[80%] my-3 mx-0 h-2 rounded-lg appearance-none cursor-pointer bg-osvauld-placeholderblack" />
 			</div>
-			<div
-				class="border-b border-osvauld-iconblack w-[90%] translate-x-4"
-			></div>
+			<div class="border-b border-osvauld-iconblack w-[90%] translate-x-4">
+			</div>
 			<div class="flex items-center justify-between px-3">
 				<span>Special characters (!&*)</span>
 				<label
 					for="specialChar"
-					class="inline-flex items-center cursor-pointer"
-				>
+					class="inline-flex items-center cursor-pointer">
 					<span class="relative">
 						<span
 							class="block w-10 h-6 {includeSpecialChars
@@ -232,21 +225,18 @@
 						<span
 							class="absolute block w-4 h-4 mt-1 ml-1 rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transform transition-transform duration-300 ease-in-out {includeSpecialChars
 								? 'bg-osvauld-plainwhite translate-x-full'
-								: 'bg-osvauld-chalkwhite'}"
-						>
+								: 'bg-osvauld-chalkwhite'}">
 							<input
 								type="checkbox"
 								id="specialChar"
 								class="absolute opacity-0 w-0 h-0"
-								bind:checked="{includeSpecialChars}"
-							/>
+								bind:checked="{includeSpecialChars}" />
 						</span>
 					</span>
 				</label>
 			</div>
-			<div
-				class="border-b border-osvauld-iconblack w-[90%] translate-x-4"
-			></div>
+			<div class="border-b border-osvauld-iconblack w-[90%] translate-x-4">
+			</div>
 			<div class="flex items-center justify-between px-3">
 				<span>Capital letters (A-Z)</span>
 				<label for="capitals" class="inline-flex items-center cursor-pointer">
@@ -259,21 +249,18 @@
 						<span
 							class="absolute block w-4 h-4 mt-1 ml-1 rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transform transition-transform duration-300 ease-in-out {includeCapital
 								? 'bg-osvauld-plainwhite translate-x-full'
-								: 'bg-osvauld-chalkwhite'}"
-						>
+								: 'bg-osvauld-chalkwhite'}">
 							<input
 								type="checkbox"
 								id="capitals"
 								class="absolute opacity-0 w-0 h-0"
-								bind:checked="{includeCapital}"
-							/>
+								bind:checked="{includeCapital}" />
 						</span>
 					</span>
 				</label>
 			</div>
-			<div
-				class="border-b border-osvauld-iconblack w-[90%] translate-x-4"
-			></div>
+			<div class="border-b border-osvauld-iconblack w-[90%] translate-x-4">
+			</div>
 			<div class="flex items-center justify-between px-3">
 				<span>Include numbers (0-9)</span>
 				<label for="numbers" class="inline-flex items-center cursor-pointer">
@@ -286,14 +273,12 @@
 						<span
 							class="absolute block w-4 h-4 mt-1 ml-1 rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transform transition-transform duration-300 ease-in-out {includeNumbers
 								? 'bg-osvauld-plainwhite translate-x-full'
-								: 'bg-osvauld-chalkwhite'}"
-						>
+								: 'bg-osvauld-chalkwhite'}">
 							<input
 								type="checkbox"
 								id="numbers"
 								class="absolute opacity-0 w-0 h-0"
-								bind:checked="{includeNumbers}"
-							/>
+								bind:checked="{includeNumbers}" />
 						</span>
 					</span>
 				</label>
@@ -303,8 +288,7 @@
 		<div class="flex justify-start items-center w-full pl-3 my-2">
 			<button
 				class="text-osvauld-fadedCancel rounded-md border border-osvauld-iconblack py-1 px-2"
-				on:click|preventDefault="{handleCancel}">Cancel</button
-			>
+				on:click|preventDefault="{handleCancel}">Cancel</button>
 		</div>
 	</div>
 </div>
