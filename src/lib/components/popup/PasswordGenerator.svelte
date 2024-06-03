97e3b2a889d06363b167cfd2507555f879145679
tony <tonyantony300@gmail.com>
2024-06-03 15:47:34 +0530

    toggle style change

--- src/lib/components/popup/PasswordGenerator.svelte
+++ src/lib/components/popup/PasswordGenerator.svelte
@@ -184,28 +184,33 @@
 				</div>
 			</div>
 		</div>
-		<div class="border-b border-osvauld-iconblack w-[90%] translate-x-4"></div>
+		<div class="border-b border-osvauld-iconblack w-[90%] translate-x-4 my-3">
+		</div>
 
 		<div class="flex flex-col gap-2">
-			<div class="flex flex-col mt-5 items-center">
-				<div class="flex justify-between items-center gap-2">
+			<div class="flex flex-col gap-2 mt-5 items-center">
+				<div class="flex justify-between items-center gap-2 w-[90%]">
 					<label class="w-[9rem]" for="lengthSlider"
-						>Length {passwordLength}</label>
+						>Length : {passwordLength}</label>
 					{#if strengthSticker === "Chad"}
-						<span class="flex justify-center items-center gap-1 w-[6rem]"
-							><Crown /> Chad</span>
+						<span class="flex justify-end items-center gap-1 w-[6rem]"
+							>Chad <Crown />
+						</span>
 					{:else if strengthSticker === "Very Strong"}
-						<span class="flex justify-center items-center gap-1 w-[6rem]"
-							><ShieldCheck />Very Strong</span>
+						<span
+							class="flex justify-end items-center gap-1 w-[6rem] text-[#A6E3A1]"
+							>Very Strong <ShieldCheck /></span>
 					{:else if strengthSticker === "Strong"}
-						<span class="flex justify-center items-center gap-1 w-[6rem]"
-							><ShieldCheck />Strong</span>
+						<span
+							class="flex justify-end items-center gap-1 w-[6rem] text-[#A6E3A1]"
+							>Strong <ShieldCheck /></span>
 					{:else if strengthSticker === "Fair"}
-						<span class="flex justify-center items-center w-[6rem]"
-							><CheckVerified />Fair</span>
+						<span class="flex justify-end items-center w-[6rem]"
+							>Fair<CheckVerified /></span>
 					{:else}
-						<span class="flex justify-center items-center gap-1 w-[6rem]"
-							><AlertOctagon />Weak</span>
+						<span
+							class="flex justify-end items-center gap-1 w-[6rem] text-[#FF6A6A]"
+							>Weak<AlertOctagon /></span>
 					{/if}
 				</div>
 				<input
@@ -214,23 +219,24 @@
 					min="8"
 					max="64"
 					bind:value="{passwordLength}"
-					class="slider w-[80%] my-3 mx-0 h-2 rounded-lg appearance-none cursor-pointer bg-osvauld-placeholderblack" />
+					class="slider w-[90%] my-3 mx-0 h-2 rounded-lg appearance-none cursor-pointer bg-osvauld-placeholderblack" />
 			</div>
-			<div class="border-b border-osvauld-iconblack w-[90%] translate-x-4">
+			<div class="border-b border-osvauld-iconblack w-[90%] translate-x-4 my-1">
 			</div>
-			<div class="flex items-center justify-between px-3">
-				<span>Special characters (!&*)</span>
+			<div
+				class="flex items-center justify-between px-4 text-osvauld-quarzowhite">
+				<span class="">Special characters (!&*)</span>
 				<label
 					for="specialChar"
 					class="inline-flex items-center cursor-pointer">
 					<span class="relative">
 						<span
-							class="block w-10 h-6 {includeSpecialChars
+							class="block w-[36px] h-[1.25rem] {includeSpecialChars
 								? 'bg-osvauld-carolinablue'
 								: 'bg-osvauld-placeholderblack'} rounded-full shadow-inner"
 						></span>
 						<span
-							class="absolute block w-4 h-4 mt-1 ml-1 rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transform transition-transform duration-300 ease-in-out {includeSpecialChars
+							class="absolute block w-4 h-4 mt-0.5 ml-0.5 rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transform transition-transform duration-300 ease-in-out {includeSpecialChars
 								? 'bg-osvauld-plainwhite translate-x-full'
 								: 'bg-osvauld-chalkwhite'}">
 							<input
@@ -242,19 +248,20 @@
 					</span>
 				</label>
 			</div>
-			<div class="border-b border-osvauld-iconblack w-[90%] translate-x-4">
+			<div class="border-b border-osvauld-iconblack w-[90%] translate-x-4 my-1">
 			</div>
-			<div class="flex items-center justify-between px-3">
+			<div
+				class="flex items-center justify-between px-4 text-osvauld-quarzowhite">
 				<span>Capital letters (A-Z)</span>
 				<label for="capitals" class="inline-flex items-center cursor-pointer">
 					<span class="relative">
 						<span
-							class="block w-10 h-6 {includeCapital
+							class="block w-[36px] h-[1.25rem] {includeCapital
 								? 'bg-osvauld-carolinablue'
 								: 'bg-osvauld-placeholderblack'} rounded-full shadow-inner"
 						></span>
 						<span
-							class="absolute block w-4 h-4 mt-1 ml-1 rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transform transition-transform duration-300 ease-in-out {includeCapital
+							class="absolute block w-4 h-4 mt-0.5 ml-0.5 rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transform transition-transform duration-300 ease-in-out {includeCapital
 								? 'bg-osvauld-plainwhite translate-x-full'
 								: 'bg-osvauld-chalkwhite'}">
 							<input
@@ -266,19 +273,20 @@
 					</span>
 				</label>
 			</div>
-			<div class="border-b border-osvauld-iconblack w-[90%] translate-x-4">
+			<div class="border-b border-osvauld-iconblack w-[90%] translate-x-4 my-1">
 			</div>
-			<div class="flex items-center justify-between px-3">
-				<span>Include numbers (0-9)</span>
+			<div
+				class="flex items-center justify-between px-4 text-osvauld-quarzowhite">
+				<span>Numbers (0-9)</span>
 				<label for="numbers" class="inline-flex items-center cursor-pointer">
 					<span class="relative">
 						<span
-							class="block w-10 h-6 {includeNumbers
+							class="block w-[36px] h-[1.25rem] {includeNumbers
 								? 'bg-osvauld-carolinablue'
 								: 'bg-osvauld-placeholderblack'} rounded-full shadow-inner"
 						></span>
 						<span
-							class="absolute block w-4 h-4 mt-1 ml-1 rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transform transition-transform duration-300 ease-in-out {includeNumbers
+							class="absolute block w-4 h-4 mt-0.5 ml-0.5 rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transform transition-transform duration-300 ease-in-out {includeNumbers
 								? 'bg-osvauld-plainwhite translate-x-full'
 								: 'bg-osvauld-chalkwhite'}">
 							<input
@@ -290,11 +298,14 @@
 					</span>
 				</label>
 			</div>
+			<div
+				class="border-b border-osvauld-iconblack w-[90%] translate-x-4 my-1.5">
+			</div>
 		</div>
 
 		<div class="flex justify-start items-center w-full pl-3 my-2">
 			<button
-				class="text-osvauld-fadedCancel rounded-md border border-osvauld-iconblack py-1 px-2"
+				class=" rounded-md border border-osvauld-iconblack py-1 px-2"
 				on:click|preventDefault="{handleCancel}">Cancel</button>
 		</div>
 	</div>
