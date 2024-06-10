d5b8b4ba8b9fe9abb1fb7c17652b959ec31c99a3
tony <tonyantony300@gmail.com>
2024-06-10 15:21:08 +0530

    setting passphrase related ui errors fixed

--- src/lib/Popup.svelte
+++ src/lib/Popup.svelte
@@ -1,59 +1,59 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import Welcome from "./components/popup/Welcome.svelte";
-	import Home from "./components/popup/Home.svelte";
-	import { Logo } from "./components/dashboard/icons";
-	import Signup from "./components/popup/Signup.svelte";
-	import Loader from "./components/dashboard/components/Loader.svelte";
-	import { sendMessage } from "./components/dashboard/helper";
+  import { onMount } from "svelte";
+  import Welcome from "./components/popup/Welcome.svelte";
+  import Home from "./components/popup/Home.svelte";
+  import { Logo } from "./components/dashboard/icons";
+  import Signup from "./components/popup/Signup.svelte";
+  import Loader from "./components/dashboard/components/Loader.svelte";
+  import { sendMessage } from "./components/dashboard/helper";
 
-	let devType = "popup";
-	let loggedIn = true;
-	let isLoaderActive = false;
-	let isSignedUp = false;
-	onMount(async () => {
-		isLoaderActive = true;
-		if (devType != "popup") openFullscreenTab();
-		const response = await sendMessage("isSignedUp");
-		isSignedUp = response.isSignedUp;
-		const checkPvtLoad = await sendMessage("checkPvtLoaded");
-		if (checkPvtLoad) {
-			loggedIn = true;
-		} else {
-			loggedIn = false;
-		}
-		isLoaderActive = false;
-		// isSignedUp.set(false);
-	});
+  let devType = "popup";
+  let loggedIn = true;
+  let isLoaderActive = false;
+  let isSignedUp = false;
+  onMount(async () => {
+    isLoaderActive = true;
+    if (devType != "popup") openFullscreenTab();
+    const response = await sendMessage("isSignedUp");
+    isSignedUp = response.isSignedUp;
+    const checkPvtLoad = await sendMessage("checkPvtLoaded");
+    if (checkPvtLoad) {
+      loggedIn = true;
+    } else {
+      loggedIn = false;
+    }
+    isLoaderActive = false;
+    // isSignedUp.set(false);
+  });
 
-	const openFullscreenTab = async () => {
-		// Send a message to the background
-		await sendMessage("openFullscreenTab");
-	};
+  const openFullscreenTab = async () => {
+    // Send a message to the background
+    await sendMessage("openFullscreenTab");
+  };
 
-	const checkAuth = (event: CustomEvent) => {
-		loggedIn = event.detail;
-	};
+  const checkAuth = (event: CustomEvent) => {
+    loggedIn = event.detail;
+  };
 </script>
 
 <main>
-	<div
-		class="w-[22.5rem] h-[36.78rem] p-2 pt-3 flex flex-col !font-sans {isSignedUp &&
-		!loggedIn
-			? 'justify-center'
-			: 'justify-start'} items-center bg-osvauld-frameblack"
-	>
-		{#if isLoaderActive}
-			<Loader />
-		{:else if !isSignedUp}
-			<Signup on:signedUp="{() => (isSignedUp = true)}" />
-		{:else if loggedIn}
-			<Home />
-		{:else}
-			<div class="mb-12 flex justify-center items-center">
-				<Logo />
-			</div>
-			<Welcome on:authenticated="{checkAuth}" />
-		{/if}
-	</div>
+  <div
+    class="w-[22.5rem] h-[36.78rem] p-2 pt-3 flex flex-col !font-sans {isSignedUp &&
+    !loggedIn
+      ? 'justify-center'
+      : 'justify-start'} items-center bg-osvauld-frameblack"
+  >
+    {#if isLoaderActive}
+      <Loader />
+    {:else if !isSignedUp}
+      <Signup on:signedUp={() => (isSignedUp = true)} />
+    {:else if loggedIn}
+      <Home />
+    {:else}
+      <div class="mb-12 flex justify-center items-center">
+        <Logo />
+      </div>
+      <Welcome on:authenticated={checkAuth} />
+    {/if}
+  </div>
 </main>
--- src/lib/components/popup/Signup.svelte
+++ src/lib/components/popup/Signup.svelte
@@ -1,51 +1,51 @@
 <script lang="ts">
-	import TempLogin from "./TempLogin.svelte";
-	import SetPassPhrase from "./SetPassPhrase.svelte";
-	import ImportPvtKey from "./ImportPvtKey.svelte";
-	import { sendMessage } from "../dashboard/helper";
-	import { createEventDispatcher } from "svelte";
+  import TempLogin from "./TempLogin.svelte";
+  import SetPassPhrase from "./SetPassPhrase.svelte";
+  import ImportPvtKey from "./ImportPvtKey.svelte";
+  import { sendMessage } from "../dashboard/helper";
+  import { createEventDispatcher } from "svelte";
 
-	let isTempLoginVerified = false;
-	let challenge = "";
-	let username = "";
-	let importPvtKeyFlag = false;
-	const dispatch = createEventDispatcher();
+  let isTempLoginVerified = false;
+  let challenge = "";
+  let username = "";
+  let importPvtKeyFlag = false;
+  const dispatch = createEventDispatcher();
 
-	const handleTempLogin = (e: any) => {
-		challenge = e.detail.challenge;
-		username = e.detail.username;
-		isTempLoginVerified = true;
-	};
+  const handleTempLogin = (e: any) => {
+    challenge = e.detail.challenge;
+    username = e.detail.username;
+    isTempLoginVerified = true;
+  };
 
-	const handleRecovery = (e: any) => {
-		importPvtKeyFlag = e.detail;
-	};
+  const handleRecovery = (e: any) => {
+    importPvtKeyFlag = e.detail;
+  };
 
-	const importPvtKey = async (e: any) => {
-		const { privateKeys, passphrase } = e.detail;
-		await sendMessage("importPvtKey", {
-			passphrase,
-			privateKeys,
-		});
-		handleSignedUp();
-	};
+  const importPvtKey = async (e: any) => {
+    const { privateKeys, passphrase } = e.detail;
+    await sendMessage("importPvtKey", {
+      passphrase,
+      privateKeys,
+    });
+    handleSignedUp();
+  };
 
-	const handleSignedUp = () => {
-		dispatch("signedUp");
-	};
+  const handleSignedUp = () => {
+    dispatch("signedUp");
+  };
 </script>
 
 <div
-	class="h-full w-[90%] flex justify-center items-center text-base font-bold text-white"
+  class="h-full w-[90%] flex justify-center items-center text-base font-bold text-white"
 >
-	{#if importPvtKeyFlag}
-		<ImportPvtKey on:submit="{(e) => importPvtKey(e)}" />
-	{:else if isTempLoginVerified}
-		<SetPassPhrase {challenge} {username} on:signedUp="{handleSignedUp}" />
-	{:else}
-		<TempLogin
-			on:setPassPhrase="{handleTempLogin}"
-			on:recovery="{handleRecovery}"
-		/>
-	{/if}
+  {#if importPvtKeyFlag}
+    <ImportPvtKey on:submit={(e) => importPvtKey(e)} />
+  {:else if isTempLoginVerified}
+    <SetPassPhrase {challenge} {username} on:signedUp={handleSignedUp} />
+  {:else}
+    <TempLogin
+      on:setPassPhrase={handleTempLogin}
+      on:recovery={handleRecovery}
+    />
+  {/if}
 </div>
