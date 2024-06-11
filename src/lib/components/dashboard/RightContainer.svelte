c06bafb5c3dd1dc3d548298ec10da93a07e94a35
tony <tonyantony300@gmail.com>
2024-06-11 10:51:03 +0530

    search from env fixed

--- src/lib/components/dashboard/RightContainer.svelte
+++ src/lib/components/dashboard/RightContainer.svelte
@@ -1,159 +1,164 @@
 <script lang="ts">
-  import CredentialList from "./credentials/CredentialList.svelte";
-  import GroupList from "./groups/GroupList.svelte";
-  import SearchModal from "./SearchModal.svelte";
-  import Environments from "./credentials/Environments.svelte";
-  import { searchObjects } from "./helper";
-  import { getSearchFields } from "./apis";
-  import { Profile, Lens, DownArrow } from "./icons";
-  import { onMount } from "svelte";
-  import {
-    selectedFolder,
-    selectedPage,
-    credentialStore,
-    searchedCredential,
-    folderStore,
-    selectedSection,
-  } from "./store";
+	import CredentialList from "./credentials/CredentialList.svelte";
+	import GroupList from "./groups/GroupList.svelte";
+	import SearchModal from "./SearchModal.svelte";
+	import Environments from "./credentials/Environments.svelte";
+	import { searchObjects } from "./helper";
+	import { getSearchFields } from "./apis";
+	import { Profile, Lens, DownArrow } from "./icons";
+	import { onMount } from "svelte";
+	import {
+		selectedFolder,
+		selectedPage,
+		credentialStore,
+		searchedCredential,
+		folderStore,
+		selectedSection,
+	} from "./store";
 
-  import ProfileModal from "./components/ProfileModal.svelte";
-  let searchResults = [];
-  let searchData = [];
-  let showModal = false;
-  let isProfileClicked = false;
-  let clickedOutside = false;
-  let top = 0;
-  let left = 0;
-  let query = "";
-  let username = "";
+	import ProfileModal from "./components/ProfileModal.svelte";
+	let searchResults = [];
+	let searchData = [];
+	let showModal = false;
+	let isProfileClicked = false;
+	let clickedOutside = false;
+	let top = 0;
+	let left = 0;
+	let query = "";
+	let username = "";
 
-  const getSearchData = async () => {
-    showModal = true;
-    const searchFieldSResponse = await getSearchFields();
-    searchData = searchFieldSResponse.data;
-    searchResults = query.length !== 0 ? searchObjects(query, searchData) : [];
-  };
+	const getSearchData = async () => {
+		showModal = true;
+		const searchFieldSResponse = await getSearchFields();
+		searchData = searchFieldSResponse.data;
+		searchResults = query.length !== 0 ? searchObjects(query, searchData) : [];
+	};
 
-  const handleInputChange = (e: any) => {
-    const query = e.type === "input" ? e.target.value : e.detail;
-    searchResults = query.length >= 1 ? searchObjects(query, searchData) : [];
-  };
+	const handleInputChange = (e: any) => {
+		const query = e.type === "input" ? e.target.value : e.detail;
+		searchResults = query.length >= 1 ? searchObjects(query, searchData) : [];
+	};
 
-  const closeModal = () => {
-    showModal = false;
-    query = "";
-    searchResults = [];
-  };
-  const handleSearchClick = (e: any) => {
-    searchedCredential.set(e.detail);
-    selectedPage.set("Folders");
-    for (const folder of $folderStore) {
-      if (folder.id === e.detail.folderId) {
-        selectedFolder.set(folder);
-        break;
-      }
-    }
-    closeModal();
-  };
+	const closeModal = () => {
+		showModal = false;
+		query = "";
+		searchResults = [];
+	};
+	const handleSearchClick = (e: any) => {
+		searchedCredential.set(e.detail);
+		selectedPage.set("Folders");
+		if (e.detail.folderType === "shared") {
+			selectedSection.set("SharedFolders");
+		} else if (e.detail.folderType === "private") {
+			selectedSection.set("PrivateFolders");
+		}
+		for (const folder of $folderStore) {
+			if (folder.id === e.detail.folderId) {
+				selectedFolder.set(folder);
+				break;
+			}
+		}
+		closeModal();
+	};
 
-  selectedPage.subscribe(() => {
-    credentialStore.set([]);
-  });
+	selectedPage.subscribe(() => {
+		credentialStore.set([]);
+	});
 
-  function handleKeyDown(event) {
-    if (event.key === "Enter") {
-      getSearchData();
-    }
-  }
+	function handleKeyDown(event) {
+		if (event.key === "Enter") {
+			getSearchData();
+		}
+	}
 
-  function profileSelectionManager(e: any) {
-    if (!clickedOutside) {
-      const rect = e.currentTarget.getBoundingClientRect();
-      top = rect.top + window.scrollY + rect.height;
-      left = rect.left + window.scrollX;
-      isProfileClicked = !isProfileClicked;
-    }
-  }
+	function profileSelectionManager(e: any) {
+		if (!clickedOutside) {
+			const rect = e.currentTarget.getBoundingClientRect();
+			top = rect.top + window.scrollY + rect.height;
+			left = rect.left + window.scrollX;
+			isProfileClicked = !isProfileClicked;
+		}
+	}
 
-  function autofocus(node) {
-    node.focus();
-  }
-  const handleProfileClose = () => {
-    isProfileClicked = false;
-    clickedOutside = true;
-    setTimeout(() => {
-      clickedOutside = false;
-    }, 100);
-  };
+	function autofocus(node) {
+		node.focus();
+	}
+	const handleProfileClose = () => {
+		isProfileClicked = false;
+		clickedOutside = true;
+		setTimeout(() => {
+			clickedOutside = false;
+		}, 100);
+	};
 
-  onMount(() => {
-    username =
-      localStorage.getItem("username") ||
-      JSON.parse(localStorage.getItem("user")).username;
-  });
+	onMount(() => {
+		username =
+			localStorage.getItem("username") ||
+			JSON.parse(localStorage.getItem("user")).username;
+	});
 </script>
 
 <div class="flex flex-col h-auto">
-  <div
-    class="h-[6rem] pr-4 flex justify-between items-center border-b border-osvauld-iconblack"
-  >
-    <div
-      class="h-[2.2rem] w-[31.25rem] px-2 mx-auto flex justify-start items-center border border-osvauld-iconblack focus-within:border-osvauld-activeBorder rounded-lg cursor-pointer"
-    >
-      <Lens />
-      <input
-        type="text"
-        class="h-[2rem] w-full bg-osvauld-frameblack border-0 text-osvauld-quarzowhite placeholder-osvauld-placeholderblack border-transparent text-base focus:border-transparent focus:ring-0 cursor-pointer"
-        placeholder="Search.."
-        use:autofocus
-        on:click={getSearchData}
-        on:input={handleInputChange}
-        bind:value={query}
-        on:keyup={handleKeyDown}
-      />
-    </div>
-    <button
-      class="profile-button min-w-[8rem] max-w-[12rem] bg-osvauld-cardshade rounded-md flex justify-around text-osvauld-fieldText items-center cursor-pointer py-1"
-      on:click|stopPropagation={profileSelectionManager}
-    >
-      <div class="flex justify-center items-center">
-        <Profile color={isProfileClicked ? "#F2F2F0" : "#85889C"} /><span
-          class="font-inter text-base overflow-hidden max-w-[6rem] text-ellipsis whitespace-nowrap {isProfileClicked
-            ? 'text-osvauld-sideListTextActive'
-            : 'text-osvauld-fieldText'} ">{username}</span
-        >
-      </div>
-      <span
-        class="transition-transform duration-100 ease-linear {isProfileClicked
-          ? 'rotate-180'
-          : 'rotate-0'}"
-      >
-        <DownArrow type={isProfileClicked ? "profileActive" : "profile"} />
-      </span>
-    </button>
-  </div>
+	<div
+		class="h-[6rem] pr-4 flex justify-between items-center border-b border-osvauld-iconblack"
+	>
+		<div
+			class="h-[2.2rem] w-[31.25rem] px-2 mx-auto flex justify-start items-center border border-osvauld-iconblack focus-within:border-osvauld-activeBorder rounded-lg cursor-pointer"
+		>
+			<Lens />
+			<input
+				type="text"
+				class="h-[2rem] w-full bg-osvauld-frameblack border-0 text-osvauld-quarzowhite placeholder-osvauld-placeholderblack border-transparent text-base focus:border-transparent focus:ring-0 cursor-pointer"
+				placeholder="Search.."
+				use:autofocus
+				on:click="{getSearchData}"
+				on:input="{handleInputChange}"
+				bind:value="{query}"
+				on:keyup="{handleKeyDown}"
+			/>
+		</div>
+		<button
+			class="profile-button min-w-[8rem] max-w-[12rem] bg-osvauld-cardshade rounded-md flex justify-around text-osvauld-fieldText items-center cursor-pointer py-1"
+			on:click|stopPropagation="{profileSelectionManager}"
+		>
+			<div class="flex justify-center items-center">
+				<Profile color="{isProfileClicked ? '#F2F2F0' : '#85889C'}" /><span
+					class="font-inter text-base overflow-hidden max-w-[6rem] text-ellipsis whitespace-nowrap {isProfileClicked
+						? 'text-osvauld-sideListTextActive'
+						: 'text-osvauld-fieldText'} ">{username}</span
+				>
+			</div>
+			<span
+				class="transition-transform duration-100 ease-linear {isProfileClicked
+					? 'rotate-180'
+					: 'rotate-0'}"
+			>
+				<DownArrow type="{isProfileClicked ? 'profileActive' : 'profile'}" />
+			</span>
+		</button>
+	</div>
 
-  {#if isProfileClicked}
-    <ProfileModal {top} {left} {username} on:close={handleProfileClose} />
-  {/if}
+	{#if isProfileClicked}
+		<ProfileModal {top} {left} {username} on:close="{handleProfileClose}" />
+	{/if}
 
-  {#if showModal}
-    <SearchModal
-      {searchResults}
-      {query}
-      on:close={closeModal}
-      on:select={handleSearchClick}
-      on:change={handleInputChange}
-      on:enter={handleKeyDown}
-    />
-  {/if}
-  <div class="h-[90vh] bg-osvauld-frameblack mr-4 overflow-hidden">
-    {#if $selectedPage === "Folders" && ($selectedSection === "SharedFolders" || $selectedSection === "PrivateFolders")}
-      <CredentialList />
-    {:else if $selectedPage === "Folders" && $selectedSection === "Environments"}
-      <Environments />
-    {:else if $selectedPage === "Groups"}
-      <GroupList />
-    {/if}
-  </div>
+	{#if showModal}
+		<SearchModal
+			{searchResults}
+			{query}
+			on:close="{closeModal}"
+			on:select="{handleSearchClick}"
+			on:change="{handleInputChange}"
+			on:enter="{handleKeyDown}"
+		/>
+	{/if}
+	<div class="h-[90vh] bg-osvauld-frameblack mr-4 overflow-hidden">
+		{#if $selectedPage === "Folders" && ($selectedSection === "SharedFolders" || $selectedSection === "PrivateFolders")}
+			<CredentialList />
+		{:else if $selectedPage === "Folders" && $selectedSection === "Environments"}
+			<Environments />
+		{:else if $selectedPage === "Groups"}
+			<GroupList />
+		{/if}
+	</div>
 </div>
--- src/lib/components/popup/SetPassPhrase.svelte
+++ src/lib/components/popup/SetPassPhrase.svelte
@@ -1,142 +1,142 @@
 <script lang="ts">
-  import Eye from "../basic/icons/eye.svelte";
-  import ClosedEye from "../basic/icons/closedEye.svelte";
-  import Loader from "../dashboard/components/Loader.svelte";
-  import { sendMessage } from "../dashboard/helper";
-  import { createEventDispatcher } from "svelte";
+	import Eye from "../basic/icons/eye.svelte";
+	import ClosedEye from "../basic/icons/closedEye.svelte";
+	import Loader from "../dashboard/components/Loader.svelte";
+	import { sendMessage } from "../dashboard/helper";
+	import { createEventDispatcher } from "svelte";
 
-  export let challenge;
-  export let username;
+	export let challenge;
+	export let username;
 
-  const dispatch = createEventDispatcher();
-  let passphrase = "";
-  let confirmPassphrase = "";
-  let showFirstPassword = false;
-  let showSecondPassword = false;
-  let showPassphraseMismatchError = false;
-  let passphraseEmpty = false;
-  let isLoaderActive = false;
+	const dispatch = createEventDispatcher();
+	let passphrase = "";
+	let confirmPassphrase = "";
+	let showFirstPassword = false;
+	let showSecondPassword = false;
+	let showPassphraseMismatchError = false;
+	let passphraseEmpty = false;
+	let isLoaderActive = false;
 
-  $: type1 = showFirstPassword ? "text" : "password";
-  $: type2 = showSecondPassword ? "text" : "password";
+	$: type1 = showFirstPassword ? "text" : "password";
+	$: type2 = showSecondPassword ? "text" : "password";
 
-  const handlePassPhraseSubmit = async () => {
-    if (passphrase.length === 0) {
-      passphraseEmpty = true;
-      isLoaderActive = false;
-      setTimeout(() => {
-        passphraseEmpty = false;
-      }, 1500);
-      return;
-    }
-    isLoaderActive = true;
-    if (passphrase === confirmPassphrase) {
-      const response = await sendMessage("savePassphrase", {
-        passphrase,
-        challenge,
-        username,
-      });
-      if (response.isSaved) {
-        isLoaderActive = false;
-        dispatch("signedUp");
-      }
-    } else {
-      showPassphraseMismatchError = true;
-      isLoaderActive = false;
-      setTimeout(() => {
-        showPassphraseMismatchError = false;
-      }, 1500);
-    }
-  };
-  function onInput(event: any, type: string) {
-    if (type === "passphrase") passphrase = event.target.value;
-    else confirmPassphrase = event.target.value;
-  }
+	const handlePassPhraseSubmit = async () => {
+		if (passphrase.length === 0) {
+			passphraseEmpty = true;
+			isLoaderActive = false;
+			setTimeout(() => {
+				passphraseEmpty = false;
+			}, 1500);
+			return;
+		}
+		isLoaderActive = true;
+		if (passphrase === confirmPassphrase) {
+			const response = await sendMessage("savePassphrase", {
+				passphrase,
+				challenge,
+				username,
+			});
+			if (response.isSaved) {
+				isLoaderActive = false;
+				dispatch("signedUp");
+			}
+		} else {
+			showPassphraseMismatchError = true;
+			isLoaderActive = false;
+			setTimeout(() => {
+				showPassphraseMismatchError = false;
+			}, 1500);
+		}
+	};
+	function onInput(event: any, type: string) {
+		if (type === "passphrase") passphrase = event.target.value;
+		else confirmPassphrase = event.target.value;
+	}
 
-  const togglePassword = (identification: boolean) => {
-    identification
-      ? (showFirstPassword = !showFirstPassword)
-      : (showSecondPassword = !showSecondPassword);
-  };
+	const togglePassword = (identification: boolean) => {
+		identification
+			? (showFirstPassword = !showFirstPassword)
+			: (showSecondPassword = !showSecondPassword);
+	};
 </script>
 
 <form
-  class="flex flex-col justify-center items-center"
-  on:submit|preventDefault={handlePassPhraseSubmit}
+	class="flex flex-col justify-center items-center"
+	on:submit|preventDefault="{handlePassPhraseSubmit}"
 >
-  <label for="passphrase" class="font-normal mt-6">Enter Passphrase</label>
+	<label for="passphrase" class="font-normal mt-6">Enter Passphrase</label>
 
-  <div
-    class="flex bg-osvauld-frameblack px-3 mt-4 border rounded-lg border-osvauld-iconblack"
-  >
-    <input
-      class="text-white bg-osvauld-frameblack border-0 tracking-wider font-normal border-transparent focus:border-transparent focus:ring-0 w-full"
-      type={type1}
-      autocomplete="off"
-      id="password"
-      on:input={(e) => onInput(e, "passphrase")}
-    />
+	<div
+		class="flex bg-osvauld-frameblack px-3 mt-4 border rounded-lg border-osvauld-iconblack"
+	>
+		<input
+			class="text-white bg-osvauld-frameblack border-0 tracking-wider font-normal border-transparent focus:border-transparent focus:ring-0 w-full"
+			type="{type1}"
+			autocomplete="off"
+			id="password"
+			on:input="{(e) => onInput(e, 'passphrase')}"
+		/>
 
-    <button
-      type="button"
-      class="flex justify-center items-center"
-      on:click={() => togglePassword(true)}
-    >
-      {#if showFirstPassword}
-        <ClosedEye />
-      {:else}
-        <Eye />
-      {/if}
-    </button>
-  </div>
-  <label for="passphrase" class="font-normal mt-6">Confirm Passphrase</label>
+		<button
+			type="button"
+			class="flex justify-center items-center"
+			on:click="{() => togglePassword(true)}"
+		>
+			{#if showFirstPassword}
+				<ClosedEye />
+			{:else}
+				<Eye />
+			{/if}
+		</button>
+	</div>
+	<label for="passphrase" class="font-normal mt-6">Confirm Passphrase</label>
 
-  <div
-    class="flex bg-osvauld-frameblack px-3 mt-4 border rounded-lg border-osvauld-iconblack"
-  >
-    <input
-      class="text-white bg-osvauld-frameblack border-0 tracking-wider font-normal border-transparent focus:border-transparent focus:ring-0 w-full"
-      type={type2}
-      autocomplete="off"
-      id="password"
-      on:input={(e) => onInput(e, "confirmPassphrase")}
-    />
+	<div
+		class="flex bg-osvauld-frameblack px-3 mt-4 border rounded-lg border-osvauld-iconblack"
+	>
+		<input
+			class="text-white bg-osvauld-frameblack border-0 tracking-wider font-normal border-transparent focus:border-transparent focus:ring-0 w-full"
+			type="{type2}"
+			autocomplete="off"
+			id="password"
+			on:input="{(e) => onInput(e, 'confirmPassphrase')}"
+		/>
 
-    <button
-      type="button"
-      class="flex justify-center items-center"
-      on:click={() => togglePassword(false)}
-    >
-      {#if showSecondPassword}
-        <ClosedEye />
-      {:else}
-        <Eye />
-      {/if}
-    </button>
-  </div>
+		<button
+			type="button"
+			class="flex justify-center items-center"
+			on:click="{() => togglePassword(false)}"
+		>
+			{#if showSecondPassword}
+				<ClosedEye />
+			{:else}
+				<Eye />
+			{/if}
+		</button>
+	</div>
 
-  {#if passphraseEmpty}
-    <span
-      class="mt-2 text-xs text-red-400 font-light {passphraseEmpty
-        ? 'visible'
-        : 'invisible'}">Passphrase Empty!</span
-    >
-  {:else}
-    <span
-      class="mt-2 text-xs text-red-400 font-light {showPassphraseMismatchError
-        ? 'visible'
-        : 'invisible'}">Passphrase doesn't match</span
-    >
-  {/if}
+	{#if passphraseEmpty}
+		<span
+			class="mt-2 text-xs text-red-400 font-light {passphraseEmpty
+				? 'visible'
+				: 'invisible'}">Passphrase Empty!</span
+		>
+	{:else}
+		<span
+			class="mt-2 text-xs text-red-400 font-light {showPassphraseMismatchError
+				? 'visible'
+				: 'invisible'}">Passphrase doesn't match</span
+		>
+	{/if}
 
-  <button
-    class="bg-osvauld-carolinablue py-2 px-10 mt-8 rounded-lg text-osvauld-ninjablack font-medium w-[150px] flex justify-center items-center whitespace-nowrap"
-    type="submit"
-  >
-    {#if isLoaderActive}
-      <Loader size={24} color="#1F242A" duration={1} />
-    {:else}
-      <span>Submit</span>
-    {/if}</button
-  >
+	<button
+		class="bg-osvauld-carolinablue py-2 px-10 mt-8 rounded-lg text-osvauld-ninjablack font-medium w-[150px] flex justify-center items-center whitespace-nowrap"
+		type="submit"
+	>
+		{#if isLoaderActive}
+			<Loader size="{24}" color="#1F242A" duration="{1}" />
+		{:else}
+			<span>Submit</span>
+		{/if}</button
+	>
 </form>
