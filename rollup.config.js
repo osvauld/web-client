2190be29e413bde517694d1ae0eb028e852864b7
abrahamgeorge8547 <abrahamgeorge8547@gmail.com>
2024-06-12 15:14:09 +0530

    unified rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,37 @@ import postcss from "rollup-plugin-postcss";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
 import os from "os";
+import fs from "fs";
+const production = !process.env.ROLLUP_WATCH;
+const buildEnv = process.env.BUILD_ENV;
+function serve() {
+  return {
+    writeBundle() {
+      // Open Brave browser with the specified URL
+      if(production) {
+      const manifestSource = buildEnv === 'firefox' ? 'public/manifests/manifest_firefox.json' : 'public/manifests/manifest_chrome.json';
+      const manifestDest = 'public/manifest.json';
+      fs.copyFileSync(manifestSource, manifestDest);
+      }else {
+        let command;
+        if (os.platform() === "linux") {
+          command = "brave  --reload-extension=public/build";
+        } else {
+          command =
+          "'/Applications/Brave Browser.app/Contents/MacOS/Brave Browser' --reload-extension=public/build";
+          
+        }
+  
+        // Open Brave browser with the specified URL
+        exec(command, (err) => {
+          if (err) {
+            console.error("Failed to open Brave:", err);
+          }
+        });
+      }
+    },
+  };
+}
 
 function buildConfig(inputFileName, outputFileName) {
   return {
@@ -54,7 +85,7 @@ export default [
   {
     input: "src/scripts/background.ts",
     output: {
-      format: "es",
+      format: buildEnv === 'firefox' ? 'iife' : 'es',
       name: "background",
       file: "public/background.js",
     },
@@ -83,6 +114,7 @@ export default [
       }),
       commonjs(),
       resolve({ browser: true, preferBuiltins: false }),
+      serve(),
     ],
     watch: {
       clearScreen: false,
