b4e5ae02d1f01c0580e2c05501b36d1a2075d5e9
Abraham George <abrahamgeorge8547@gmail.com>
2024-09-21 01:06:05 +0530

    create share creds abstracted

--- src/scripts/wasm.js
+++ src/scripts/wasm.js
@@ -729,6 +729,26 @@ export function get_public_key() {
 	}
 }
 
+/**
+ * @param {any} input
+ * @returns {any}
+ */
+export function create_share_creds_payload(input) {
+	try {
+		const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
+		wasm.create_share_creds_payload(retptr, addHeapObject(input));
+		var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
+		var r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
+		var r2 = getDataViewMemory0().getInt32(retptr + 4 * 2, true);
+		if (r2) {
+			throw takeObject(r1);
+		}
+		return takeObject(r0);
+	} finally {
+		wasm.__wbindgen_add_to_stack_pointer(16);
+	}
+}
+
 function handleError(f, args) {
 	try {
 		return f.apply(this, args);
