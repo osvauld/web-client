5b777190f33ae900157d7b1162dedb45c7f897b1
tony <tonyantony300@gmail.com>
2024-09-21 14:31:42 +0530

    all founctional

--- extension/src/lib/components/popup/ImportPvtKey.svelte
+++ extension/src/lib/components/popup/ImportPvtKey.svelte
@@ -15,14 +15,6 @@
 	let isLoaderActive = false;
 	let isPassphraseAcceptable = false;
 
-	const togglePasswordVisibility = (isInitialResponse: boolean) => {
-		if (isInitialResponse) {
-			showPassword = !showPassword;
-		} else {
-			showReenteredPassword = !showReenteredPassword;
-		}
-	};
-
 	$: submitDisabled =
 		passphrase.length === 0 || passphrase !== reenteredPassPhrase;
 
@@ -54,28 +46,34 @@
 </script>
 
 <div
-	class="flex flex-col justify-center items-center text-osvauld-sheffieldgrey">
+	class="flex flex-col justify-center items-center text-osvauld-sheffieldgrey"
+>
 	<div>
 		<h3 class="font-medium text-3xl mb-6 text-osvauld-ownerText">
 			Account Recovery
 		</h3>
 	</div>
 	<label for="privateKey" class="font-normal mt-6 mb-2"
-		>Enter Recovery string</label>
+		>Enter Recovery string</label
+	>
 	<textarea
 		class="text-osvauld-quarzowhite bg-osvauld-frameblack border border-osvauld-iconblack tracking-wider font-light text-sm font-mono focus:border-osvauld-iconblack focus:ring-0 resize-none w-[300px] min-h-[6rem] max-h-[10rem] rounded-lg scrollbar-thin overflow-y-scroll"
 		id="privateKey"
-		on:input="{(e) => handleInputChange(e, 'recovery')}"></textarea>
+		on:input="{(e) => handleInputChange(e, 'recovery')}"
+	></textarea>
 
 	<label for="passphrase" class="font-normal mt-6 mb-2"
-		>Enter New Passphrase</label>
+		>Enter New Passphrase</label
+	>
 	<div
-		class="flex justify-between items-center bg-osvauld-frameblack px-3 border rounded-lg border-osvauld-iconblack w-[300px]">
+		class="flex justify-between items-center bg-osvauld-frameblack px-3 border rounded-lg border-osvauld-iconblack w-[300px]"
+	>
 		<input
 			class="text-white bg-osvauld-frameblack border-0 tracking-wider font-normal border-transparent focus:border-osvauld-iconblack focus:ring-0 active:outline-none focus:ring-offset-0"
 			type="{showPassword ? 'text' : 'password'}"
 			id="passphrase"
-			on:input="{(e) => handleInputChange(e, 'passphrase')}" />
+			on:input="{(e) => handleInputChange(e, 'passphrase')}"
+		/>
 
 		{#if isPassphraseAcceptable}
 			<span class="pr-2"><Tick /></span>
@@ -83,7 +81,8 @@
 		<button
 			type="button"
 			class="flex justify-center items-center"
-			on:click="{() => togglePasswordVisibility(true)}">
+			on:click="{() => togglePasswordVisibility(true)}"
+		>
 			{#if showPassword}
 				<ClosedEye />
 			{:else}
@@ -93,20 +92,24 @@
 	</div>
 	<PasswordStrengthValidator {passphrase} bind:isPassphraseAcceptable />
 	<label for="passphrase" class="font-normal mt-2 mb-2"
-		>Confirm New Passphrase</label>
+		>Confirm New Passphrase</label
+	>
 	<div
-		class="flex justify-between items-center bg-osvauld-frameblack px-3 border rounded-lg border-osvauld-iconblack w-[300px]">
+		class="flex justify-between items-center bg-osvauld-frameblack px-3 border rounded-lg border-osvauld-iconblack w-[300px]"
+	>
 		<input
 			class="text-white bg-osvauld-frameblack border-0 tracking-wider font-normal border-transparent focus:border-osvauld-iconblack focus:ring-0 active:outline-none focus:ring-offset-0"
 			type="{showReenteredPassword ? 'text' : 'password'}"
 			disabled="{!isPassphraseAcceptable}"
 			id="passphrase"
-			on:input="{(e) => handleConfirmationInputChange(e)}" />
+			on:input="{(e) => handleConfirmationInputChange(e)}"
+		/>
 
 		<button
 			type="button"
 			class="flex justify-center items-center"
-			on:click="{() => togglePasswordVisibility(false)}">
+			on:click="{() => togglePasswordVisibility(false)}"
+		>
 			{#if showReenteredPassword}
 				<ClosedEye />
 			{:else}
@@ -121,7 +124,8 @@
 			: 'bg-osvauld-carolinablue text-osvauld-ninjablack'} py-2 px-10 mt-8 rounded-lg font-medium w-[150px] flex justify-center items-center whitespace-nowrap"
 		type="button"
 		disabled="{submitDisabled}"
-		on:click="{handleSubmit}">
+		on:click="{handleSubmit}"
+	>
 		{#if isLoaderActive}
 			<Loader />
 		{:else}
