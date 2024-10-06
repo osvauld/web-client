e48ef0d0ea747abab107810a179fbb13fdce46a7
tony <tonyantony300@gmail.com>
2024-10-06 17:00:52 +0530

    removed credential form now only mod comp

--- extension/src/lib/components/dashboard/credentials/CredentialEditorMod.svelte
+++ extension/src/lib/components/dashboard/credentials/CredentialEditorMod.svelte
@@ -1,14 +1,6 @@
 <script lang="ts">
 	import { onMount, createEventDispatcher } from "svelte";
-	import {
-		updateCredentialStore,
-		resetCredentialStore,
-		CredentialStoreData,
-		credentialFieldsStore,
-		credentialStore,
-	} from "./credentialTypes/credentialStore";
 	import { setCredentialStore } from "../../../store/storeHelper";
-	import CredentialForm from "./credentialTypes/CredentialForm.svelte";
 	import {
 		fetchCredentialUsersForDataSync,
 		fetchFolderUsersForDataSync,
@@ -18,27 +10,45 @@
 	import { selectedFolder } from "../store";
 	import { Folder } from "../../../dtos/folder.dto";
 	import {
-		AddCredentialPayload,
-		UpdateCredentialPayload,
-		CredentialFieldComponentProps,
-	} from "../dtos";
+		isUpdateCredentialPayload,
+		isAddCredentialPayload,
+		credentialFieldsUpdater,
+	} from "./credentialTypes/fieldHelper";
+
+	import {
+		CredentialStoreData,
+		updateCredentialStore,
+	} from "./credentialTypes/credentialStore";
+	import LoginCredential from "./credentialTypes/LoginCredential.svelte";
+	import CustomCredential from "./credentialTypes/CustomCredential.svelte";
+	import NoteCredential from "./credentialTypes/NoteCredential.svelte";
+	import { fly, blur } from "svelte/transition";
+	import { ClosePanel } from "../icons";
 
 	export let edit: boolean = false;
 	export let credentialId: string = "";
 	let isCredentialNamed = true;
 	export let name: string = "";
 	export let description: string = "";
-	export let credentialType: string = "";
-	export let credentialFields: CredentialFieldComponentProps[] = [
-		{ fieldName: "Username", fieldValue: "", sensitive: false },
-		{ fieldName: "Password", fieldValue: "", sensitive: true },
-		{ fieldName: "URL", fieldValue: "https://", sensitive: false },
-		{ fieldName: "TOTP", fieldValue: "", sensitive: true },
-	];
+	let credentialType: string = "";
 	let isLoaderActive: boolean = false;
 	let successView: boolean = false;
 	let operationFailed: boolean = false;
 	let failureMessage: string = "";
+	let credentialFields;
+	const templateTypes = [
+		"Login",
+		"Credit/DebitCard",
+		"Contact",
+		"Digital Wallet",
+		"SSH Key",
+		"Database",
+		"PIN",
+		"Note",
+		"Bank Account",
+		"Social Media",
+		"API Key",
+	];
 
 	const dispatcher = createEventDispatcher();
 
@@ -58,19 +68,7 @@
 		}
 	});
 
-	$: console.log("credentialFieldsStore in editorMod", $credentialFieldsStore);
-
-	function isUpdateCredentialPayload(
-		data: any,
-	): data is UpdateCredentialPayload {
-		return "credentialId" in data;
-	}
-
-	function isAddCredentialPayload(data: any): data is AddCredentialPayload {
-		return !("credentialId" in data);
-	}
-
-	const handleSubmit = async (
+	const handleSubmitParent = async (
 		credentialData: CredentialStoreData,
 		edit: boolean,
 		credentialId?: string,
@@ -117,16 +115,83 @@
 		}
 	};
 
+	const credentialTypeSelection = (type) => {
+		credentialFields = credentialFieldsUpdater(type);
+		credentialType = type;
+	};
+
+	const handleSubmit = () => {
+		// updateCredentialStore({ credentialFields: $credentialFieldsStore });
+		// handleSubmitParent($credentialStore, edit, credentialId);
+	};
+
 	const handleCancel = () => {
-		resetCredentialStore();
+		// resetCredentialStore();
 		dispatcher("close");
 		// Close dialog or navigate away
 	};
 </script>
 
-<CredentialForm
-	{edit}
-	{isCredentialNamed}
-	{failureMessage}
-	onSubmit="{handleSubmit}"
-	onCancel="{handleCancel}" />
+<form on:submit|preventDefault="{handleSubmit}">
+	<div
+		class="bg-osvauld-frameblack rounded-3xl border border-osvauld-iconblack z-50"
+		in:fly
+		out:blur>
+		<div class="flex justify-between items-center px-12 py-6">
+			<div>
+				<span class="text-[28px] font-sans font-normal"
+					>{credentialType === ""
+						? "Add secret to Osvauld"
+						: `Add ${credentialType}`}</span>
+			</div>
+			<div>
+				<!-- {#if edit}
+					<button
+						class="bg-osvauld-frameblack p-4"
+						on:click="{deleteCredential}"
+						type="button"><BinIcon /></button>
+				{/if} -->
+
+				<button
+					class="bg-osvauld-frameblack p-4"
+					on:click="{handleCancel}"
+					type="button"><ClosePanel /></button>
+			</div>
+		</div>
+		<div class="border-b border-osvauld-iconblack w-full"></div>
+
+		{#if credentialType !== ""}
+			<CustomCredential {credentialFields} {edit} {isCredentialNamed} />
+		{:else}
+			<div class="w-full grid grid-cols-2 grid-rows-6 gap-3 pl-4 py-2">
+				{#each templateTypes as type}
+					<button
+						on:click="{() => credentialTypeSelection(type)}"
+						type="button"
+						class="w-[20rem] p-2 pl-4 text-lg rounded-lg flex items-center cursor-pointer hover:text-osvauld-sideListTextActive hover:bg-osvauld-fieldActive text-osvauld-fieldText'"
+						><span class="text-base font-light">{type} </span></button
+					>{/each}
+			</div>
+		{/if}
+
+		{#if failureMessage !== ""}
+			<span class="text-red-500 text-base font-normal mr-3 mb-6"
+				>{failureMessage}</span>
+		{/if}
+
+		<div class="border-b border-osvauld-iconblack w-full my-2"></div>
+
+		<div class="flex justify-end items-center mx-10 py-2">
+			<button
+				class="px-3 py-1.5 mb-6 whitespace-nowrap text-osvauld-fadedCancel bg-osvauld-frameblack hover:bg-osvauld-cardshade flex justify-center items-center rounded-md hover:text-osvauld-textActive text-base font-normal"
+				type="button"
+				on:click="{handleCancel}">Cancel</button>
+			<button
+				type="submit"
+				class="px-3 py-1.5 mb-6 whitespace-nowrap flex justify-center items-center ml-3 border border-osvauld-textActive text-osvauld-textActive hover:bg-osvauld-carolinablue hover:text-osvauld-frameblack hover:border-osvauld-carolinablue font-normal text-base rounded-md">
+				<span class="w-[8.6rem]"
+					>{edit ? "Save Changes" : "Add credential"}</span>
+			</button>
+		</div>
+	</div>
+</form>
--- extension/src/lib/components/dashboard/credentials/credentialTypes/CustomCredential.svelte
+++ extension/src/lib/components/dashboard/credentials/credentialTypes/CustomCredential.svelte
@@ -1,50 +1,43 @@
 <script lang="ts">
 	import AddLoginFields from "../AddLoginFields.svelte";
-	import { Add, BinIcon } from "../../icons";
+	import { Add } from "../../icons";
 	import { credentialStore, credentialFieldsStore } from "./credentialStore";
-	import { AddCredentialField } from "../../dtos";
+	import type { AddCredentialField } from "../../dtos";
 	import { fieldEditHandler } from "./fieldHelper";
-	export let edit;
-	export let hoveredIndex;
+
+	export let edit: boolean;
+	export let credentialFields: AddCredentialField[];
 	export let isCredentialNamed = true;
 
-	const newField = {
+	let hoveredIndex: number | null = null;
+
+	const newField: AddCredentialField = {
 		fieldName: "",
 		fieldValue: "",
 		sensitive: false,
 	};
 
 	function addNewField() {
-		credentialFieldsStore.update((fields) => [...fields, newField]);
+		credentialFields = [...credentialFields, newField];
 	}
 
-	const triggerSensitiveBubble = (index: number, isEnter: boolean) => {
-		isEnter ? (hoveredIndex = index) : (hoveredIndex = null);
-	};
+	function triggerSensitiveBubble(index: number, isEnter: boolean) {
+		hoveredIndex = isEnter ? index : null;
+	}
 
-	// $: isCredentialNamed = $credentialStore.name.length !== 0;
-
-	const removeField = (index: number) => {
-		// const field = $credentialFieldsStore[index];
-		// if (field && field.fieldId) {
-		// 	const fieldId = field.fieldId;
-		// 	if (typeof fieldId === "string") {
-		// 		deletedFields.push(fieldId);
-		// 	}
-		// }
-		const filterRemaining = $credentialFieldsStore.filter(
-			(_, indexToRemove) => index !== indexToRemove,
+	function removeField(indexToRemove: number) {
+		credentialFields = credentialFields.filter(
+			(_, index) => index !== indexToRemove,
 		);
-		credentialFieldsStore.set(filterRemaining);
-	};
+	}
 </script>
 
 <div class="mx-6">
-	<div class="min-h-[32vh] max-h-[35vh] overflow-y-scroll scrollbar-thin z-50">
+	<div class="min-h-[32vh] max-h-[35vh] overflow-y-auto scrollbar-thin z-50">
 		<input
 			class="w-[78%] mb-2 mt-4 ml-6 pl-4 bg-osvauld-frameblack
-   border rounded-md text-base text-osvauld-quarzowhite font-normal
-   focus:border-osvauld-activeBorder flex focus:ring-0 placeholder-osvauld-iconblack {!isCredentialNamed
+border rounded-md text-base text-osvauld-quarzowhite font-normal
+focus:border-osvauld-activeBorder flex focus:ring-0 placeholder-osvauld-iconblack {!isCredentialNamed
 				? 'border-red-500'
 				: 'border-osvauld-iconblack '}"
 			id="name"
@@ -52,7 +45,7 @@
 			placeholder="Enter Credential name...."
 			autocomplete="off"
 			bind:value="{$credentialStore.name}" />
-		{#each $credentialFieldsStore as field, index}
+		{#each credentialFields as field, index (index)}
 			{#if field.fieldName !== "Domain"}
 				<AddLoginFields
 					{field}
@@ -60,23 +53,22 @@
 					{hoveredIndex}
 					on:select="{(e) =>
 						triggerSensitiveBubble(e.detail.index, e.detail.identifier)}"
-					on:remove="{(e) => removeField(e.detail)}"
-					on:change="{() => {
-						fieldEditHandler(field, $credentialFieldsStore, edit);
-					}}" />
+					on:remove="{(e) => removeField(e.detail)}" />
 			{/if}
 		{/each}
 	</div>
+
 	<div class="flex mr-24">
 		<button
 			class="py-2 m-4 bg-osvauld-addfieldgrey flex-1 flex justify-center items-center rounded-md text-osvauld-chalkwhite border-2 border-dashed border-osvauld-iconblack"
 			on:click="{addNewField}"
 			type="button">
-			<Add color="{'#6E7681'}" />
+			<Add color="#6E7681" />
 		</button>
 	</div>
 </div>
-<div class=" mx-6 pl-3 flex justify-start items-center mb-5">
+
+<div class="mx-6 pl-3 flex justify-start items-center mb-5">
 	<textarea
 		rows="2"
 		class="w-5/6 mt-4 h-auto min-h-[6rem] max-h-[10rem] bg-osvauld-frameblack rounded-lg scrollbar-thin border-osvauld-iconblack resize-none text-base focus:border-osvauld-iconblack focus:ring-0"
