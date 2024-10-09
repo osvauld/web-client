9e4dc63f37885319b4d0b700e0908b16383d9bef
Abraham George <abrahamgeorge8547@gmail.com>
2024-10-09 19:54:09 +0530

    Changed file name

---
+++ extension/src/lib/Suggestions.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import SuccessView from "./components/content/components/SuccessView.svelte";
+	import FolderView from "./components/content/components/FolderView.svelte";
+	import SuggestionView from "./components/content/components/SuggestionView.svelte";
+	import { ModifiedEvent } from "./dtos/event.dto";
+
+	let folderView = false;
+	let successView = false;
+	let isSuccess = false;
+	let iframeCustomEvent: ModifiedEvent;
+
+	onMount(async () => {
+		window.addEventListener("message", (event) => {
+			if (event.data.id !== "osvauld") {
+				return;
+			}
+			iframeCustomEvent = {
+				data: event.data,
+				source: event.source as MessageEventSource,
+				origin: event.origin,
+			};
+		});
+	});
+</script>
+
+<div
+	class="w-full h-[200px] bg-osvauld-frameblack text-base text-white overflow-hidden">
+	<span class="font-extrabold text-2xl text-white pb-4">osvauld</span>
+	{#if successView}
+		<SuccessView {isSuccess} {iframeCustomEvent} />
+	{:else if folderView}
+		<FolderView
+			{iframeCustomEvent}
+			bind:isSuccess
+			bind:triggerSuccess="{successView}" />
+	{:else}
+		<SuggestionView {iframeCustomEvent} bind:value="{folderView}" />
+	{/if}
+</div>
