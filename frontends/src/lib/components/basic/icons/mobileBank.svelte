c43f0c6e4728beb99f2b914579f7d863150e265c
tony <tonyantony300@gmail.com>
2024-10-25 12:46:23 +0530

    more icons added

---
+++ frontends/src/lib/components/basic/icons/mobileBank.svelte
@@ -0,0 +1,12 @@
+<svg
+	width="25"
+	height="24"
+	viewBox="0 0 25 24"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg">
+	<path
+		fill-rule="evenodd"
+		clip-rule="evenodd"
+		d="M20 16.5H21.5V18H3.5V16.5H5V8.5H3.5V4.66L12.5 2.5L21.5 4.66V8.5H20V16.5ZM12.5 4.04L5 5.84V7H20V5.84L12.5 4.04ZM6.5 8.5V16.5H9.5V8.5H6.5ZM11 8.5V16.5H14V8.5H11ZM15.5 8.5V16.5H18.5V8.5H15.5ZM2.5 21V19.5H22.5V21H2.5Z"
+		fill="#85889C"></path>
+</svg>
--- frontends/src/mobile/mobile.ts
+++ frontends/src/mobile/mobile.ts
@@ -1,7 +1,7 @@
-import App from "../lib/Mobile2.svelte";
+import App from "../lib/Mobile.svelte";
 import "../tailwind.css";
 const app = new App({
-    target: document.body,
+	target: document.body,
 });
 
 export default app;
