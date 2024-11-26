340afce3f462c8d76c7532c642f4578ff7caddf3
Abraham George <abrahamgeorge8547@gmail.com>
2024-11-26 14:56:08 +0530

    connection integrated

--- frontends/src/mobile/components/layouts/ProfileLayout.svelte
+++ frontends/src/mobile/components/layouts/ProfileLayout.svelte
@@ -1,1 +1,21 @@
-<h1 class="text-white">Profile layout</h1>
+<script lang="ts">
+	import Initiator from "../views/Initiator.svelte";
+	import Acceptor from "../views/Acceptor.svelte";
+	let initiator = false;
+	const changeProfile = () => {
+		initiator = !initiator;
+	};
+</script>
+
+<button class="text-white" on:click="{changeProfile}">
+	{#if initiator}
+		<span>Initiator</span>
+	{:else}
+		<span>Acceptor</span>
+	{/if}
+</button>
+{#if initiator}
+	<Initiator />
+{:else}
+	<Acceptor />
+{/if}
