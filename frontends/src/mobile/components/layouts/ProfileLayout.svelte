d3474fb5824f1e1778b725ce4612178977ff84d4
tony <tonyantony300@gmail.com>
2024-12-16 14:32:23 +0530

    search tab css done

--- frontends/src/mobile/components/layouts/ProfileLayout.svelte
+++ frontends/src/mobile/components/layouts/ProfileLayout.svelte
@@ -2,19 +2,29 @@
 	import Initiator from "../views/Initiator.svelte";
 	import Acceptor from "../views/Acceptor.svelte";
 	import { slide } from "svelte/transition";
+	import MobileLeftArrow from "../../../icons/mobileLeftArrow.svelte";
+	import { currentLayout } from "../../store/mobile.ui.store";
 
 	let isInitiator = false;
 
 	const toggleMode = () => {
 		isInitiator = !isInitiator;
 	};
+
+	const goBack = () => {
+		currentLayout.set("home");
+	};
 </script>
 
 <main
-	class="w-screen h-screen bg-mobile-bgPrimary flex flex-col relative pt-[48px] pb-[60px] overflow-hidden">
+	class="w-screen h-screen bg-mobile-bgPrimary flex flex-col relative pb-[60px] overflow-hidden">
 	<!-- Tab Navigation -->
-	<div
-		class="w-full h-[48px] flex justify-between items-center px-4 fixed top-0">
+	<nav class="w-full h-[48px] px-3 flex items-center gap-2 flex-shrink-0">
+		<button on:click="{goBack}" class="p-2.5 rounded-lg bg-mobile-bgSeconary">
+			<MobileLeftArrow />
+		</button>
+	</nav>
+	<div class="w-full h-[48px] flex justify-between items-center px-4">
 		<div class="w-full flex border-b border-mobile-bgHighlight">
 			<button
 				class="flex-1 h-[48px] text-center {!isInitiator
