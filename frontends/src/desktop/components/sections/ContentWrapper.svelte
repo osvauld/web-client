96d5373ab794e1133618a76939df3d0589e15335
tony <tonyantony300@gmail.com>
2025-01-10 14:11:50 +0530

    add cred from all vaults also restored

--- frontends/src/desktop/components/sections/ContentWrapper.svelte
+++ frontends/src/desktop/components/sections/ContentWrapper.svelte
@@ -20,29 +20,30 @@
 			class="text-4xl font-light text-osvauld-sideListTextActive flex justify-between items-center gap-3 capitalize max-w-1/2 truncate">
 			{$selectedCategory ? $selectedCategory : $currentVault.name}
 		</h1>
-		{#if $currentVault.id !== "all"}
-			<div class="gap-4 flex justify-between items-center ml-3">
+
+		<div class="gap-4 flex justify-between items-center ml-3">
+			{#if $currentVault.id !== "all"}
 				<button
 					class="p-2"
 					on:click|stopPropagation="{() => deleteConfirmationModal.set(true)}"
 					><Bin color="#FF6A6A" /></button>
-				<span><Menu /></span>
-				<button
-					class="bg-osvauld-frameblack text-osvauld-textPassive flex justify-center items-center py-2 px-3 text-sm rounded-md ml-4"
-					aria-label="Sort by latest">
-					<span class="mr-2 pl-2">Latest</span>
-					<span><DownArrow type="common" /></span>
-				</button>
-				<button
-					class="rounded-md py-1.5 px-4 mx-2 flex justify-center items-center whitespace-nowrap text-sm border text-osvauld-textActive border-osvauld-iconblack hover:text-osvauld-frameblack hover:bg-osvauld-carolinablue transition-colors"
-					on:mouseenter="{() => (addCredentialHovered = true)}"
-					on:mouseleave="{() => (addCredentialHovered = false)}"
-					on:click="{() => addCredentialModal.set(true)}">
-					<span class="mr-2">Add New Credential</span>
-					<Add color="{addCredentialHovered ? '#000' : '#A3A4B5'}" />
-				</button>
-			</div>
-		{/if}
+			{/if}
+			<span><Menu /></span>
+			<button
+				class="bg-osvauld-frameblack text-osvauld-textPassive flex justify-center items-center py-2 px-3 text-sm rounded-md ml-4"
+				aria-label="Sort by latest">
+				<span class="mr-2 pl-2">Latest</span>
+				<span><DownArrow type="common" /></span>
+			</button>
+			<button
+				class="rounded-md py-1.5 px-4 mx-2 flex justify-center items-center whitespace-nowrap text-sm border text-osvauld-textActive border-osvauld-iconblack hover:text-osvauld-frameblack hover:bg-osvauld-carolinablue transition-colors"
+				on:mouseenter="{() => (addCredentialHovered = true)}"
+				on:mouseleave="{() => (addCredentialHovered = false)}"
+				on:click="{() => addCredentialModal.set(true)}">
+				<span class="mr-2">Add New Credential</span>
+				<Add color="{addCredentialHovered ? '#000' : '#A3A4B5'}" />
+			</button>
+		</div>
 	</div>
 	<CredentialList />
 </div>
