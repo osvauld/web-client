25cddca4cde19d269047a6ba16420e1616f388f8
tony <tonyantony300@gmail.com>
2025-01-13 16:18:23 +0530

    scrollbar removed notes and fields appear

--- frontends/src/desktop/components/views/CredentialList.svelte
+++ frontends/src/desktop/components/views/CredentialList.svelte
@@ -129,7 +129,7 @@
 			<ImportModal on:close="{closeImportModal}" />
 		</div>
 	{/if}
-	<div class="h-full overflow-y-auto overflow-x-hidden pr-4">
+	<div class="h-full overflow-y-auto overflow-x-hidden pr-1 scrollbar-none">
 		<div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3">
 			{#each Array(getColumnCount()) as _, colIndex}
 				<div class="flex flex-col gap-3">
